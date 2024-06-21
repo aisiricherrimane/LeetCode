4aa11cbb0d4d07039eff6393bcf1b7b2e9715cc1
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-06-21 10:07:37 +0530

    :ballot_box_with_check: first-bad-version [Runtime: 38 ms; Memory: 16.4 MB]

---
+++ interactive/first-bad-version.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 0 
+        r = n - 1
+        while l <= r:
+            m = (l + r) // 2
+            if isBadVersion(m) == False:
+                l = m + 1
+            else:
+                r = m - 1
+        return l
+
+
+
+        
