f25022caf0e4705f1083236db72b5c8b5e820b51
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-07-03 08:50:39 +0530

    :ballot_box_with_check: first-unique-character-in-a-string [Runtime: 285 ms; Memory: 16.9 MB]

--- hash-table/first-unique-character-in-a-string.py
+++ hash-table/first-unique-character-in-a-string.py
@@ -1,12 +1,11 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        temp = {}
-        for i in s:
-            temp[i] = 1 + temp.get(i, 0)
-        for i in range(len(s)):
-            if temp[s[i]] == 1:
+        n = len(s)
+        for i in range(n):
+            if s[i] not in s[:i] and s[i] not in s[i + 1:]:
                 return i
-                break
         return -1
+
+         
 
         
