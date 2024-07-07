6fe2ee3e5c0e1c4d1ef1a5e31872f05e7f2df88a
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-07-07 10:50:15 +0530

    :ballot_box_with_check: minimum-size-subarray-sum [Runtime: 208 ms; Memory: 30.4 MB]

--- array/minimum-size-subarray-sum.py
+++ array/minimum-size-subarray-sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         l = 0
+        r = 0
         sumi = 0
         length = float("inf")
-        for r in range(len(nums)):
+        while r < len(nums):
             sumi += nums[r]
             while sumi >= target:
                 length = min(length, r - l + 1)
                 sumi -= nums[l]
                 l += 1
-        return 0 if length == float("inf") else length
+            r += 1
+        return 0 if length == float('inf') else length
 
 
             
