3c372322fb5db2febaccc8ec537f321dfb5e814f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-07-17 23:51:02 +0530

    :ballot_box_with_check: minimum-depth-of-binary-tree [Runtime: 246 ms; Memory: 43.6 MB]

---
+++ tree/minimum-depth-of-binary-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if root.right and root.left:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+        elif root.right:
+            return 1 + self.minDepth(root.right)
+        elif root.left:
+            return 1 + self.minDepth(root.left)
+        else:
+            # If leaf return it's height.
+            return 1
