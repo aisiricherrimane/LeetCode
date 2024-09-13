6ff9618dd0b891f29321157aa421b7892cddcfa6
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-09-12 22:41:49 -0400

    LC-accounts-merge [Runtime: N/A; Memory: N/A]

--- array/accounts-merge.py
+++ array/accounts-merge.py
@@ -1,41 +1,49 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, x):
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+    def union(self, e1, e2):
+        p1 = self.find(e1)
+        p2 = self.find(e2)
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+        
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        email_to_parent = {}  # Union-Find parent map
-        email_to_name = {}    # Maps email to the name (person's name)
+        uf = UnionFind(len(accounts))
+        email_to_ind = {}
+
+        for i, a in enumerate(accounts):
+            for e in a[1:]:
+                if e in email_to_ind:
+                    uf.union(i, email_to_ind[e])
+                else:
+                    email_to_ind[e] = i
         
-        def find(email):
-            if email_to_parent[email] != email:
-                email_to_parent[email] = find(email_to_parent[email])
-            return email_to_parent[email]
+        emailGroup = colllections.defaultdict(list)
+        for e, i in email_to_ind.items():
+            leader = uf.find(i)
+            emailGroup[leader].append(e)
         
-        def union(email1, email2):
-            root1 = find(email1)
-            root2 = find(email2)
-            if root1 != root2:
-                email_to_parent[root2] = root1
-        
-        # Step 1: Union all emails of each account
-        for account in accounts:
-            name = account[0]
-            first_email = account[1]
-            for email in account[1:]:
-                email_to_parent.setdefault(email, email)  # Initialize if not present
-                email_to_name[email] = name  # Store the name corresponding to this email
-                union(first_email, email)   # Union all emails with the first email
-        
-        # Step 2: Group emails by their root parent
-        merged_accounts = {}
-        for email in email_to_parent:
-            root_email = find(email)  # Find the root email for this email
-            if root_email not in merged_accounts:
-                merged_accounts[root_email] = []
-            merged_accounts[root_email].append(email)
-        
-        # Step 3: Prepare the result
-        result = []
-        for root_email, emails in merged_accounts.items():
-            result.append([email_to_name[root_email]] + sorted(emails))
-        
-        return result
+        res = []
+        for i, emails in emailGroup.items():
+            name = accounts[i][0]
+            res.append([name] + sorted(emailGroup[i])) # array concat
+        return res
+
+
 
         
