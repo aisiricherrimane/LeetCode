9dee3a9c89430f830340ae09d25be96037bf444e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-09-14 16:04:39 -0400

    LC-minimum-cost-to-reach-destination-in-time [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/minimum-cost-to-reach-destination-in-time.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
+        adjM = collections.defaultdict(list)
+        
+        for x, y, t in edges:
+            adjM[x].append([y, t])
+            adjM[y].append([x, t])
+        
+        minH = [[passingFees[0], 0, 0]]
+        visit = set()
+        res = float('inf')
+
+        while minH:
+            cost, time, node = heapq.heappop(minH)
+            if node in visit or time > maxTime:
+                continue
+            if node == len(passingFees) - 1:
+                res = min(res, cost)
+            visit.add(node)
+            
+            for nei_city, nei_time in adjM[node]:
+                if nei_city is not visit:
+                    heapq.heappush(minH, [cost + passingFees[nei_city], time + nei_time, nei_city])
+        
+        return res if res != float('inf') else -1
+
+
+
+
+
+
+
+        
