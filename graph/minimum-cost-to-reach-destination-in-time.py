439aafddfdf29d185db9a34db5298e9aa7f6f5a7
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-09-14 16:16:33 -0400

    LC-minimum-cost-to-reach-destination-in-time [Runtime: N/A; Memory: N/A]

--- graph/minimum-cost-to-reach-destination-in-time.py
+++ graph/minimum-cost-to-reach-destination-in-time.py
@@ -1,33 +1,45 @@
+import heapq
+import collections
+
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
         adjM = collections.defaultdict(list)
         
+        # Create adjacency list from edges
         for x, y, t in edges:
             adjM[x].append([y, t])
             adjM[y].append([x, t])
         
-        minH = [[passingFees[0], 0, 0]]
+        # Min-heap (priority queue), where we store [cost, time, node]
+        minH = [[passingFees[0], 0, 0]]  # start from node 0
+        
+        # Set to track visited states as (node, time)
         visit = set()
+        
         res = float('inf')
 
         while minH:
             cost, time, node = heapq.heappop(minH)
-            if node in visit:
-                continue
-            if node == len(passingFees) - 1 and time <= maxTime:
-                res = min(res, cost)
-            visit.add(node)
             
+            # If the time exceeds maxTime, skip this path
+            if time > maxTime:
+                continue
+            
+            # If we've reached the destination and within maxTime, update result
+            if node == len(passingFees) - 1:
+                res = min(res, cost)
+            
+            # Mark the node and time as visited
+            visit.add((node, time))
+            
+            # Process neighbors
             for nei_city, nei_time in adjM[node]:
-                if nei_city not in visit and time + nei_time <= maxTime:
-                    heapq.heappush(minH, [cost + passingFees[nei_city], time + nei_time, nei_city])
-        
+                new_time = time + nei_time
+                new_cost = cost + passingFees[nei_city]
+
+                # Allow revisiting only if the new state (node, time) is not visited
+                if (nei_city, new_time) not in visit:
+                    heapq.heappush(minH, [new_cost, new_time, nei_city])
+
+        # If a valid result was found, return it, otherwise return -1
         return res if res != float('inf') else -1
-
-
-
-
-
-
-
-        
