7dc98dff6c7d8d0b7f827374dba832d221a4ab17
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-09-14 16:20:36 -0400

    LC-minimum-cost-to-reach-destination-in-time [Runtime: N/A; Memory: N/A]

--- dynamic-programming/minimum-cost-to-reach-destination-in-time.py
+++ dynamic-programming/minimum-cost-to-reach-destination-in-time.py
@@ -1,45 +1,33 @@
-import heapq
-import collections
-
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
         adjM = collections.defaultdict(list)
         
-        # Create adjacency list from edges
         for x, y, t in edges:
             adjM[x].append([y, t])
             adjM[y].append([x, t])
         
-        # Min-heap (priority queue), where we store [cost, time, node]
-        minH = [[passingFees[0], 0, 0]]  # start from node 0
-        
-        # Set to track visited states as (node, time)
+        minH = [[passingFees[0], 0, 0]]
         visit = set()
-        
         res = float('inf')
 
         while minH:
             cost, time, node = heapq.heappop(minH)
-            
-            # If the time exceeds maxTime, skip this path
-            if time > maxTime:
+            if node in visit:
                 continue
-            
-            # If we've reached the destination and within maxTime, update result
-            if node == len(passingFees) - 1:
+            if node == len(passingFees) - 1 and time <= maxTime:
                 res = min(res, cost)
+            visit.add(node)
             
-            # Mark the node and time as visited
-            visit.add((node, time))
-            
-            # Process neighbors
             for nei_city, nei_time in adjM[node]:
-                new_time = time + nei_time
-                new_cost = cost + passingFees[nei_city]
-
-                # Allow revisiting only if the new state (node, time) is not visited
-                if (nei_city, new_time) not in visit:
-                    heapq.heappush(minH, [new_cost, new_time, nei_city])
-
-        # If a valid result was found, return it, otherwise return -1
+                if nei_city not in visit and time + nei_time <=:
+                    heapq.heappush(minH, [cost + passingFees[nei_city], time + nei_time, nei_city])
+        
         return res if res != float('inf') else -1
+
+
+
+
+
+
+
+        
