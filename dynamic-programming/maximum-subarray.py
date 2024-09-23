294d37e5524dc7846cf33d13a71b30d3b712ddbd
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-09-23 11:13:27 -0400

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-subarray.py
+++ dynamic-programming/maximum-subarray.py
@@ -2,13 +2,13 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         ans = float('-inf')
         curr_sum = 0
-        res = 0
         for i in range(len(nums)):
             curr_sum += nums[i]
-            if nums[i] > curr_sum:
-                curr_sum = nums[i]
-            res = max(res, curr_sum)
-        return res
+            
+            if curr_sum < 0:
+                curr_sum = 0
+            ans = max(ans, curr_sum)
+        return ans
 
 
 
