b0ab0501dfe0891e318f54e5bbd07df18e4d91b2
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-09-23 11:16:10 -0400

    LC-maximum-subarray [Runtime: 568 ms; Memory: 31.1 MB]

--- dynamic-programming/maximum-subarray.py
+++ dynamic-programming/maximum-subarray.py
@@ -3,10 +3,7 @@ class Solution:
         ans = float('-inf')
         curr_sum = 0
         for i in range(len(nums)):
-            curr_sum += nums[i]
-            
-            if curr_sum < 0:
-                curr_sum = 0
+            curr_sum = max(nums[i], curr_sum + nums[i])
             ans = max(ans, curr_sum)
         return ans
 
