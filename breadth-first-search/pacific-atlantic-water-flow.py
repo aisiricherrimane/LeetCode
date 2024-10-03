c329c6ffa18bde28a257177b471b44747bd52fb6
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-02 20:13:50 -0400

    LC-pacific-atlantic-water-flow [Runtime: 218 ms; Memory: 18.2 MB]

--- breadth-first-search/pacific-atlantic-water-flow.py
+++ breadth-first-search/pacific-atlantic-water-flow.py
@@ -2,33 +2,30 @@ class Solution:
     def pacificAtlantic(self, grid: List[List[int]]) -> List[List[int]]:
         pac = set()
         atl = set()
-        
         row = len(grid)
         col = len(grid[0])
 
         def dfs(r, c, visit, prevH):
-            if r < 0 or c < 0 or r == row or c == col or (r,c) in visit or grid[r][c] < prevH:
-                return 
-            visit.add((r,c))
-            dfs(r + 1, c, visit, grid[r][c])
-            dfs(r - 1, c, visit, grid[r][c])
-            dfs(r, c + 1, visit, grid[r][c])
-            dfs(r, c - 1, visit, grid[r][c])
+            if r >= 0 and r < row and c >= 0 and c < col and (r, c) not in visit and grid[r][c] >= prevH:
+                visit.add((r, c))
+                dfs(r + 1, c, visit, grid[r][c])
+                dfs(r - 1, c, visit, grid[r][c])
+                dfs(r, c + 1, visit, grid[r][c])
+                dfs(r, c - 1, visit, grid[r][c])
 
+        
         for r in range(row):
             dfs(r, 0, pac, grid[r][0])
             dfs(r, col - 1, atl, grid[r][col - 1])
-
         for c in range(col):
             dfs(0, c, pac, grid[0][c])
-            dfs(row - 1, c, atl, grid[row - 1][c]) 
-
+            dfs(row - 1, c, atl, grid[row - 1][c])
+        
         res = []
         for r in range(row):
             for c in range(col):
-                if (r,c) in pac and (r, c) in atl:
-                    res.append((r, c))
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
         return res
-        
-            
+
         
