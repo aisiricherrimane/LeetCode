afc9d1d194bd3e735c0a80d3faefde8df58eba1c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-29 14:12:36 -0400

    LC-group-anagrams [Runtime: 66 ms; Memory: 21.8 MB]

--- string/group-anagrams.py
+++ string/group-anagrams.py
@@ -1,13 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        temp = collections.defaultdict(list)
+        temp = defaultdict(list)
 
         for word in strs:
             count = [0] * 26
             for w in word:
-                count[ord('a') - ord(w)] += 1
+                count[ord(w) - ord('a')] += 1
             temp[tuple(count)].append(word)
         res = []
-        for t in temp.values():
-            res.append(t)
+        for a in temp.values():
+            res.append(a)
         return res
+
+        
