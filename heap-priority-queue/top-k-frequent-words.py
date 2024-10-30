fad76e0afa4e518be8292e4b60afa6cb972fffbc
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-30 18:50:59 -0400

    LC-top-k-frequent-words [Runtime: 0 ms; Memory: 16.8 MB]

--- heap-priority-queue/top-k-frequent-words.py
+++ heap-priority-queue/top-k-frequent-words.py
@@ -1,17 +1,11 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        temp = {}
-        for w in words:
-            temp[w] = 1 + temp.get(w, 0)
-        minH = []
-        for w, c in temp.items():
-            heapq.heappush(minH, [-1 * c, w])
-        res = []
-        while len(res) < k:
-            res.append(heapq.heappop(minH)[1])
-        return res
+        cnt = Counter(words)
+        print(cnt)
+        heap = [(-freq, word) for word, freq in cnt.items()]
+        heapify(heap)
+
+        return [heappop(heap)[1] for _ in range(k)]
+
         
-
-
-
         
