a35c282d7c6c001a24cc2eedf6d44e0ffa3829df
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-31 16:06:07 -0400

    LC-best-time-to-buy-and-sell-stock [Runtime: 78 ms; Memory: 25.9 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
-        low = prices[0]
-        for price in prices:
-            if price - low > res:
-                res = max(res, price - low)
-            low = min(low, price)
-        return res
+        profit = 0
+        lowest = prices[0]
 
-        
+        for price in prices:
+            if price < lowest:
+                lowest = min(lowest, price)
+            profit = max(profit, price - lowest)
+        return profit
