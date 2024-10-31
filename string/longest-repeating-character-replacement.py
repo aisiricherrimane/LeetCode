7c14769ccdf681059a88b644147a0284087f2d30
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-31 16:24:24 -0400

    LC-longest-repeating-character-replacement [Runtime: 46 ms; Memory: 16.6 MB]

--- string/longest-repeating-character-replacement.py
+++ string/longest-repeating-character-replacement.py
@@ -1,15 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         res = 0
-        maxf = 0
         l = 0
-        count = {}
+        maxf = 0
+        temp = {}
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
+            temp[s[r]] = 1 + temp.get(s[r], 0)
+            maxf = max(maxf, temp[s[r]])
 
-            while ((r - l + 1) - maxf) > k:
-                count[s[l]] -= 1
+            while (r - l + 1) - maxf > k:
+                temp[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1)
-        return res        
+        return res
+
+            
+
+
+        
