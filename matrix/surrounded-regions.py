2757d4f0ff0cb633da12c2a33ce5c2f4f55e0c7e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-01 10:30:46 -0400

    LC-surrounded-regions [Runtime: 3 ms; Memory: 20.7 MB]

--- matrix/surrounded-regions.py
+++ matrix/surrounded-regions.py
@@ -3,43 +3,38 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # basci declaration
-        rows = len(board)
-        cols = len(board[0])
+        # Edge case check
+        if not board or not board[0]:
+            return
+        
+        rows, cols = len(board), len(board[0])
 
-        # dfs
+        # dfs function to mark connected 'O's from borders as 'T'
         def dfs(r, c):
             board[r][c] = 'T'
-            directions = [(1, 0), (-1, 0), (0, 1), (0 ,-1)]
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
             for dr, dc in directions:
-                nR, nC = dr + r, dc + c
-                if nR >= 0 and nR < rows and nC >= 0 and nC < cols and board[nR][nC] == 'O':
-                    dfs(nR, nC)
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == 'O':
+                    dfs(nr, nc)
 
-        # for r
+        # Traverse the borders and mark 'O's connected to borders
         for r in range(rows):
             if board[r][0] == 'O':
                 dfs(r, 0)
-            elif board[r][cols - 1] == 'O':
+            if board[r][cols - 1] == 'O':
                 dfs(r, cols - 1)
-        # for c
+
         for c in range(cols):
             if board[0][c] == 'O':
                 dfs(0, c)
-            elif board[rows - 1][c] == 'O':
+            if board[rows - 1][c] == 'O':
                 dfs(rows - 1, c)
-        
-        # compleet traverse
+
+        # Flip all 'O's to 'X' and all 'T's back to 'O'
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
                 elif board[r][c] == 'O':
                     board[r][c] = 'X'
-
-        # return
-
-
-
-
-        
