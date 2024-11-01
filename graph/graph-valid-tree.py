51aa1fd1c5debfb345375ff887e37c325909a5e4
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-01 15:52:24 -0400

    LC-graph-valid-tree [Runtime: N/A; Memory: N/A]

---
+++ graph/graph-valid-tree.py
@@ -0,0 +1,8 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges)>= n:
+            return False
+        else:
+            return True
+
+        
