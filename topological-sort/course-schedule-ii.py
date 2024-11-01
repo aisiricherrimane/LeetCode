cd596a8912f34e9265980735864c221d4c2e379d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-01 11:29:22 -0400

    LC-course-schedule-ii [Runtime: 3 ms; Memory: 18.7 MB]

--- topological-sort/course-schedule-ii.py
+++ topological-sort/course-schedule-ii.py
@@ -1,22 +1,23 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = {i:[] for i in range(numCourses)}
+        course_map = {i:[] for i in range(numCourses)}
 
-        for crs, pre in prerequisites:
-            adj[crs].append(pre)
-
+        for c, p in prerequisites:
+            course_map[c].append(p)
+        
+        
         visit = set()
         visited = set()
         output = []
-
         def dfs(crs):
             if crs in visited:
                 return True
             if crs in visit:
                 return False
             visit.add(crs)
-            for nei in adj[crs]:
-                if not dfs(nei): return False
+            for pre in course_map[crs]:
+                if not dfs(pre):
+                    return False
             visit.remove(crs)
             visited.add(crs)
             output.append(crs)
@@ -25,5 +26,6 @@ class Solution:
         for i in range(numCourses):
             if not dfs(i):
                 return []
-        return output
+        return output 
+
         
