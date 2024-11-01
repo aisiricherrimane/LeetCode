9942c5bab26e9b8de77124f866a30f6822267c0c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-31 20:48:23 -0400

    LC-time-based-key-value-store [Runtime: N/A; Memory: N/A]

---
+++ design/time-based-key-value-store.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = collections.defaultdict(lambda: defaultdict(str))
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key][timestamp] = value
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+        for t in range(timestamp, 0, -1):
+            if t in self.store[key]:
+                return str(self.store[key][t])
+            else:
+                continue
+                
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
