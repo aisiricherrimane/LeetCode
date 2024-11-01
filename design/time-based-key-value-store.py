ed5f49920e28bcccf56422280dc1d632f6bcf8d3
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-10-31 20:53:16 -0400

    LC-time-based-key-value-store [Runtime: 48 ms; Memory: 73.6 MB]

--- design/time-based-key-value-store.py
+++ design/time-based-key-value-store.py
@@ -3,18 +3,17 @@ import collections
 class TimeMap:
 
     def __init__(self):
-        self.store = collections.defaultdict(lambda: defaultdict(str))
+        self.store = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # Only set if the timestamp is not already in the dictionary
-        if self.store[key][timestamp] == "":
-            self.store[key][timestamp] = value
+        self.store[key].append((timestamp, value))
+    
 
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.store:
             return ""
-        # Start from the given timestamp and go backward to find the nearest one
-        for t in range(timestamp, -1, -1):
-            if t in self.store[key]:
-                return self.store[key][t]
+       
+        for t, v in reversed(self.store[key]):
+            if t <= timestamp:
+                return v
         return ""
