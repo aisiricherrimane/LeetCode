da00685285413523a144c614133673b5a1d5d5a8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-01 16:12:17 -0400

    LC-graph-valid-tree [Runtime: N/A; Memory: N/A]

--- graph/graph-valid-tree.py
+++ graph/graph-valid-tree.py
@@ -1,8 +1,26 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges)>= n:
-            return False
-        else:
+        if not n:
             return True
+        
+        adj = defaultdict(list)
+        for i, v in edges:
+            adj[i].append(v)
+            adj[v].append(i)
+
+        visit = set()
+        def dfs(i, prev):
+            if i in visit:
+                return False
+            visit.add(i)
+            for j in adj[i]:
+                if j == prev:
+                    continue
+                if not dfs(j, i):
+                    return False
+            return True
+            
+        return dfs(0, -1) 
+
 
         
