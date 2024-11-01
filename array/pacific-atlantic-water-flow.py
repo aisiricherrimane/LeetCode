e0ec9cc20c4dcfe0b9d0bad79c124ea100102b02
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-01 09:18:06 -0400

    LC-pacific-atlantic-water-flow [Runtime: 33 ms; Memory: 18.4 MB]

--- array/pacific-atlantic-water-flow.py
+++ array/pacific-atlantic-water-flow.py
@@ -1,31 +1,28 @@
 class Solution:
-    def pacificAtlantic(self, grid: List[List[int]]) -> List[List[int]]:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         pac = set()
         atl = set()
-        row = len(grid)
-        col = len(grid[0])
+        rows = len(heights)
+        cols = len(heights[0])
 
-        def dfs(r, c, visit, prevH):
-            if r >= 0 and r < row and c >= 0 and c < col and (r, c) not in visit and grid[r][c] >= prevH:
-                visit.add((r, c))
-                dfs(r + 1, c, visit, grid[r][c])
-                dfs(r - 1, c, visit, grid[r][c])
-                dfs(r, c + 1, visit, grid[r][c])
-                dfs(r, c - 1, visit, grid[r][c])
-
+        def dfs(r, c, group, prevH):
+            if r >= 0 and r < rows and c >= 0 and c < cols and (r, c) not in group and heights[r][c] >= prevH:
+                group.add((r, c))
+                dfs(r + 1, c, group, heights[r][c])
+                dfs(r - 1, c, group, heights[r][c])
+                dfs(r, c - 1, group, heights[r][c])
+                dfs(r, c + 1, group, heights[r][c])
         
-        for r in range(row):
-            dfs(r, 0, pac, grid[r][0])
-            dfs(r, col - 1, atl, grid[r][col - 1])
-        for c in range(col):
-            dfs(0, c, pac, grid[0][c])
-            dfs(row - 1, c, atl, grid[row - 1][c])
-        
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows - 1][c])
         res = []
-        for r in range(row):
-            for c in range(col):
+        for r in range(rows):
+            for c in range(cols):
                 if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
+                    res.append((r, c))
         return res
-
         
