9150678335f5e2474a0ba664f112a4f7cb07c077
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-02 12:27:01 -0400

    LC-insert-delete-getrandom-o1-duplicates-allowed [Runtime: 116 ms; Memory: 72.4 MB]

--- design/insert-delete-getrandom-o1-duplicates-allowed.py
+++ design/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -2,25 +2,29 @@ class RandomizedCollection:
 
     def __init__(self):
         self.store = []
-        self.ind = collections.defaultdict(list)
+        self.ind = defaultdict(list)
 
     def insert(self, val: int) -> bool:
-        self.ind[val].append(len(self.store))
         self.store.append(val)
+        self.ind[val].append(len(self.store) - 1)
 
         return len(self.ind[val]) == 1
 
     def remove(self, val: int) -> bool:
-        if val not in self.ind or not self.ind[val]:
+        # check if not present and return False
+        if val not in self.ind:
             return False
 
+        # last_element and get its ind 
         last_element = self.store[-1]
-        ind_to_switch = self.ind[val].pop(0)
-        if ind_to_switch != len(self.store) - 1:
-            self.store[ind_to_switch] = last_element
-
-            self.ind[last_element].remove(len(self.store) - 1)
-            self.ind[last_element].append(ind_to_switch )
+        last_element_ind = len(self.store) - 1
+        # get vals ind and val
+        val_ind = self.ind[val].pop(0)
+        # check if v_ind != l_ind
+        if val_ind != last_element_ind:
+            self.store[val_ind] = last_element
+            self.ind[last_element].append(val_ind)
+            self.ind[last_element].remove(last_element_ind)
         self.store.pop()
 
         if not self.ind[val]:
@@ -30,6 +34,7 @@ class RandomizedCollection:
 
     def getRandom(self) -> int:
         return choice(self.store)
+
         
         
 
