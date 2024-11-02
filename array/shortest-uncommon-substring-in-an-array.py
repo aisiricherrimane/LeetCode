2996b4150d1dbe880df1dfe8670a0926717b886b
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-02 11:10:22 -0400

    LC-shortest-uncommon-substring-in-an-array [Runtime: 195 ms; Memory: 17.6 MB]

--- array/shortest-uncommon-substring-in-an-array.py
+++ array/shortest-uncommon-substring-in-an-array.py
@@ -2,62 +2,25 @@ class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
         result = []
         hashmap = defaultdict(list)
-        all_substring_frequency = defaultdict(int)
-        
-        for index in range(len(arr)):
-            hashmap[index] = []
-            current_substrings = set()
+        substring_freq = defaultdict(int)
 
+        for index in range(len(arr)):
+            substrings = set()
             for i in range(len(arr[index])):
                 for j in range(i, len(arr[index])):
-                    substring = arr[index][i:j + 1]
-                  
-
-                    if substring not in current_substrings:
-                        hashmap[index].append(substring)
-                        all_substring_frequency[substring] += 1
-                        current_substrings.add(substring)
+                    subs = arr[index][i:j + 1]
+                    
+                    if subs not in substrings:
+                        hashmap[index].append(subs)
+                        substring_freq[subs] += 1
+                        substrings.add(subs)
+        
         for key in hashmap:
             result.append('')
-            for substrings in sorted(hashmap[key], key = lambda x:(len(x), x)):
-                if all_substring_frequency[substrings] == 1:
-                    result[key] = substrings
+            for s in sorted(hashmap[key], key = lambda x:(len(x), x)):
+                if substring_freq[s] == 1:
+                    result[key] = s
                     break
         return result
-        
 
-        '''
-        result = []
-		hashmap = defaultdict(list)
-		all_substrings_frequency = defaultdict(int)
 
-		for index in range(len(arr)):
-
-			hashmap[index] = []
-			current_substrings = set()
-
-			for i in range(len(arr[index])):
-				for j in range(i, len(arr[index])):
-
-					substring = arr[index][i:j + 1]
-
-					if substring not in current_substrings:
-
-						hashmap[index].append(substring)
-
-						all_substrings_frequency[substring] = all_substrings_frequency[substring] + 1
-						current_substrings.add(substring)
-
-		for key in hashmap:
-
-			result.append('')
-
-			for substring in sorted(hashmap[key] , key = lambda x:(len(x), x)):
-
-				if all_substrings_frequency[substring] == 1:
-
-					result[key] = substring
-					break
-
-		return result
-        '''
