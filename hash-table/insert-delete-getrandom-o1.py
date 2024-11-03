471152a556e9c87d8f932be2252f57f76cd67800
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-03 07:53:00 -0500

    LC-insert-delete-getrandom-o1 [Runtime: 137 ms; Memory: 56.4 MB]

--- hash-table/insert-delete-getrandom-o1.py
+++ hash-table/insert-delete-getrandom-o1.py
@@ -1,32 +1,33 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.store = []
         self.ind = {}
-        
+        self.store = []
+
     def insert(self, val: int) -> bool:
         if val not in self.ind:
             self.store.append(val)
-            self.ind[val] = (len(self.store) - 1)
+            self.ind[val] = len(self.store) - 1
             return True
         else:
             return False
+
     def remove(self, val: int) -> bool:
         if val not in self.ind:
             return False
+
         last_element = self.store[-1]
-        ind = self.ind[val]
-        self.store[ind] = last_element
-        self.ind[last_element] = ind
-        
+        last_element_ind = len(self.store) - 1
+        val_ind = self.ind[val]
+        self.store[val_ind] = last_element
+        self.ind[last_element] = val_ind
+
         self.store.pop()
         del self.ind[val]
         return True
-        
-    def getRandom(self) -> int:
-        return choice(self.store)
-       
 
+    def getRandom(self) -> int:
+        return random.choice(self.store)
         
 
 
