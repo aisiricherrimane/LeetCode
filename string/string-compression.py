335c384e306c8b88452b631004e7c89b2791ea3d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-03 08:36:31 -0500

    LC-string-compression [Runtime: 3 ms; Memory: 16.7 MB]

--- string/string-compression.py
+++ string/string-compression.py
@@ -1,19 +1,20 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        i = 0 
+        i = 0
         ans = 0
-
         while i < len(chars):
             letter = chars[i]
             count = 0
             while i < len(chars) and chars[i] == letter:
                 count += 1
                 i += 1
+
             chars[ans] = letter
             ans += 1
+
             if count > 1:
                 for d in str(count):
                     chars[ans] = d
                     ans += 1
         return ans
-
+        
