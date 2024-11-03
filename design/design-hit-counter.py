101066e390a225156cc6a2ffd1f04a0a9681df07
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-03 08:07:55 -0500

    LC-design-hit-counter [Runtime: 0 ms; Memory: 16.7 MB]

--- design/design-hit-counter.py
+++ design/design-hit-counter.py
@@ -1,14 +1,19 @@
 class HitCounter:
 
     def __init__(self):
-        self.queue = deque()
+        self.q = deque()
+        self.count = 0
+
     def hit(self, timestamp: int) -> None:
-        self.queue.append(timestamp)
+        self.count += 1
+        self.q.append([timestamp, count])
         
     def getHits(self, timestamp: int) -> int:
-        while self.queue and timestamp - self.queue[0] >= 300:
-            self.queue.popleft()
-        return len(self.queue)
+        while self.q and timestamp - self.q[0][0] >= 300:
+            self.q.popleft()
+            self.count -= 1
+        return self.count 
+
 # Your HitCounter object will be instantiated and called as such:
 # obj = HitCounter()
 # obj.hit(timestamp)
