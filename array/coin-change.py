f197766c86f772ace88d53a760983359bf9151cb
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-03 08:58:54 -0500

    LC-coin-change [Runtime: 359 ms; Memory: 16.9 MB]

--- array/coin-change.py
+++ array/coin-change.py
@@ -1,12 +1,11 @@
 class Solution:
-   
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
 
-        for a in range(1, amount + 1):  # Start from 1 since dp[0] is already set
+        for a in range(1, amount + 1):
             for c in coins:
-                if a >= c and dp[a] > 1 + dp[a - c]:
+                if a - c >= 0 and dp[a] > 1 + dp[a - c]:
                     dp[a] = 1 + dp[a - c]
-
-        return dp[amount] if dp[amount] != (amount + 1) else -1
+        return dp[amount] if dp[amount] != amount + 1 else -1
+        
