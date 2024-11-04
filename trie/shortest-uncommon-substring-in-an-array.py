1779e129be6f9692082d08f185d793bd38a436ad
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-04 09:20:53 -0500

    LC-shortest-uncommon-substring-in-an-array [Runtime: N/A; Memory: N/A]

--- trie/shortest-uncommon-substring-in-an-array.py
+++ trie/shortest-uncommon-substring-in-an-array.py
@@ -1,27 +1,22 @@
 class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
-        hashmap = defaultdict(list)
+        map_ind_to_sub = defaultdict(list)
         substring_freq = defaultdict(int)
-
+        
         for index in range(len(arr)):
-            subs = set()
+            sub_set = set()
             for i in range(len(arr[index])):
-                for j in range(i, len(arr[index])):
-                    temp = arr[index][i: j + 1]
-                    if temp not in subs:
-                        hashmap[index].append(temp)
-                        substring_freq[temp] += 1
-                        subs.add(temp)
+                for j in range(i + 1, len(arr[index])):
+                    s = arr[index][i:j+1]
+                    if s not in sub_set:
+                        map_ind_to_sub[index].append(s)
+                        substring_freq[s] += 1
+                        sub_set.add(s)
         res = []
-        for index in hashmap.keys():
+        for index in map_ind_to_sub:
             res.append('')
-            for substring in sorted(hashmap[index], key = lambda x:(len(x), x)):
-                if substring_freq[substring] == 1:
-                    res[index] = substring
+            for s in sorted(map_ind_to_sub[index], key=lambda x:(len(x), x)):
+                if substring_freq[s] == 1:
+                    res[index] = s
                     break
         return res
-
-
-
-
-        
