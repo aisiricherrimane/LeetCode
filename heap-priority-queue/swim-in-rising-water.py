bfae4685e01fc279e8039a119b9069207b90b997
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-04 11:04:48 -0500

    LC-swim-in-rising-water [Runtime: N/A; Memory: N/A]

---
+++ heap-priority-queue/swim-in-rising-water.py
@@ -0,0 +1,19 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        minH = [[grid[0][0], 0, 0]]
+        visit = set()
+
+        while minH:
+            height, r, c = heapq.heappop(minH)
+
+            if r == n - 1 and c == n - 1:
+                return height
+            visit.add((r, c))
+            directions = [[0,1],[1,0],[0, -1], [-1, 0]]
+            for dr, dc in directions:
+                if 0 <= dr+r < n and 0 <= dc+c < n and (dr + r, dc + c) not in visit:
+                    heapq.heappush(minH, [max(height, grid[dr + r][dc + c]), dr+r, dc+c])
+        
+
+
