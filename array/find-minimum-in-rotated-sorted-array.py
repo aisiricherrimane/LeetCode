7bfa28ab25a6ea9f55bb0345aefc2184cf28ce8e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-04 17:29:41 -0500

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-minimum-in-rotated-sorted-array.py
+++ array/find-minimum-in-rotated-sorted-array.py
@@ -2,17 +2,15 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         l = 0
         r = len(nums) - 1
-        res = nums[l]
+        low = nums[l]
         while l <= r:
-            if nums[l] < nums[r]:
-                return min(res, nums[l])
-            mid = (r + l) // 2
-            res = min(res, nums[mid])
-
+            if nums[l] <= nums[r]:
+                return min(low, nums[l])
+            mid = (l + (r - l)) // 2
+            low = min(low, nums[mid])
+            
             if nums[l] <= nums[mid]:
                 l = mid + 1
             else:
                 r = mid - 1
-        return res
-
-        
+                
