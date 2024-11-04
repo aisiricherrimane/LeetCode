e1e0fe93ded8229268fc2446b62c4ae87ad696af
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-03 21:15:07 -0500

    LC-reconstruct-itinerary [Runtime: N/A; Memory: N/A]

--- depth-first-search/reconstruct-itinerary.py
+++ depth-first-search/reconstruct-itinerary.py
@@ -9,7 +9,7 @@ class Solution:
         res = ['JFK']
         def dfs(city):
             if len(res) == len(tickets)+1:
-                return res
+                return True
             if city not in adj:
                 return False
             temp = list(adj[city])
