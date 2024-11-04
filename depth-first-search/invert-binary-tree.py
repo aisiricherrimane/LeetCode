ae419518a4b6e588ee1053d32761b5023f97942b
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-04 18:04:26 -0500

    LC-invert-binary-tree [Runtime: 0 ms; Memory: 16.5 MB]

--- depth-first-search/invert-binary-tree.py
+++ depth-first-search/invert-binary-tree.py
@@ -7,13 +7,11 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return 
+            return None
         temp = root.right
         root.right = root.left
         root.left = temp
-        
-        self.invertTree(root.left)
         self.invertTree(root.right)
+        self.invertTree(root.left)
         return root
-        
         
