200c842a025a69d8e3766de72758bba3a69197d9
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-05 16:25:01 -0500

    LC-insert-delete-getrandom-o1-duplicates-allowed [Runtime: 123 ms; Memory: 72.4 MB]

--- array/insert-delete-getrandom-o1-duplicates-allowed.py
+++ array/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -13,21 +13,18 @@ class RandomizedCollection:
         if val not in self.ind:
             return False
         last_element = self.store[-1]
-        last_ind = len(self.store) - 1
-        val_ind = self.ind[val].pop(0)
+        last_element_ind = len(self.store) - 1
+        val_ind = self.ind[val].pop()
 
-        if val_ind != last_ind:
+        if last_element_ind != val_ind:
             self.store[val_ind] = last_element
-            self.ind[last_element].remove(last_ind)
+            self.ind[last_element].remove(last_element_ind)
             self.ind[last_element].append(val_ind)
+        
         self.store.pop()
-
         if not self.ind[val]:
             del self.ind[val]
-
         return True
-        
-
     def getRandom(self) -> int:
         return random.choice(self.store)
         
