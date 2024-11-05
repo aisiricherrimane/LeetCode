d24543fc617f89f46bcfd250867473506e6c3d3e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-05 17:11:20 -0500

    LC-shortest-uncommon-substring-in-an-array [Runtime: 139 ms; Memory: 17.4 MB]

--- array/shortest-uncommon-substring-in-an-array.py
+++ array/shortest-uncommon-substring-in-an-array.py
@@ -1,39 +1,35 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
-        substrings_freq = defaultdict(int)
-
+        substring_freq = defaultdict(int)
+        
         for string in arr:
-            substrings_set = set()
-            for l in range(1, len(string) + 1):
-                for i in range(len(string) - l + 1):
-                    substring = string[i:i + l]
-                    if substring not in substrings_set:
-                        substrings_freq[substring] += 1
-                        substrings_set.add(substring)
+            for length in range(1, len(string) + 1):
+                substring_seen = set()
+                for i in range(len(string) - length + 1):
+                    substring = string[i:i+length]
+                    if substring not in substring_seen:
+                        substring_freq[substring] += 1
+                        substring_seen.add(substring)
         
         res = [''] * len(arr)
+
         for index, string in enumerate(arr):
             found = False
-            for l in range(1, len(string) + 1):
-                substrings = []
-                for i in range(len(string) - l + 1):
-                    substring = string[i:i + l]
-                    substrings.append(substring)
-                substrings = sorted(substrings)
-
-                for s in substrings:
-                    if substrings_freq[s] == 1:
+            for length in range(1, len(string) + 1):
+                substring_seen = []
+                for i in range(len(string) - length + 1):
+                    substring = string[i:i+length]
+                    substring_seen.append(substring)
+                
+                substring_seen = sorted(substring_seen)
+                for s in substring_seen:
+                    if substring_freq[s] == 1:
                         res[index] = s
                         found = True
                         break
                 if found:
                     break
         return res
-
-            
-                
+                    
 
         
