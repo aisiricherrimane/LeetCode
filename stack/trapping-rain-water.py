4ac63f41200979b6bf9f5ae6bd7e26501b3fcdbf
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-05 09:09:46 -0500

    LC-trapping-rain-water [Runtime: 11 ms; Memory: 18.4 MB]

--- stack/trapping-rain-water.py
+++ stack/trapping-rain-water.py
@@ -1,18 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l = 0
+        l = 0 
         r = len(height) - 1
         maxl = height[l]
         maxr = height[r]
-        val = 0
+        res = 0
         while l < r:
             if maxl < maxr:
                 l += 1
                 maxl = max(maxl, height[l])
-                val += maxl - height[l]
+                res += maxl - height[l]
             else:
                 r -= 1
                 maxr = max(maxr, height[r])
-                val += maxr - height[r]
+                res += maxr - height[r]
+        return res
 
-        return val
+        
