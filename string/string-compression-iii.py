83854fbd32fc7405af4c82acd5025fa11b759bdc
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-05 16:46:11 -0500

    LC-string-compression-iii [Runtime: 1295 ms; Memory: 18.5 MB]

--- string/string-compression-iii.py
+++ string/string-compression-iii.py
@@ -2,16 +2,13 @@ class Solution:
     def compressedString(self, word: str) -> str:
         i = 0
         res = ''
-
         while i < len(word):
             letter = word[i]
             count = 0
             while i < len(word) and word[i] == letter:
                 count += 1
                 i += 1
-                if count == 9:
+                if count > 8:
                     break
-            res += str(count)
-            res += letter
-
+            res = res + str(count) + letter
         return res
