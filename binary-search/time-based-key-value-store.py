2b049c802f26e7d98c089aeae663faa49eb88bed
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-06 10:32:03 -0500

    LC-time-based-key-value-store [Runtime: N/A; Memory: N/A]

--- binary-search/time-based-key-value-store.py
+++ binary-search/time-based-key-value-store.py
@@ -4,16 +4,11 @@ class TimeMap:
 
     def __init__(self):
         self.store = defaultdict(list)
-
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.store[key].append([timestamp, value])
-
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store:
-            return ""
-
-        for t, v in reversed(self.store[key]):
-                if t <= timestamp:
-                    return v
-        return ""
+        for t, v in self.store[key]:
+            if t <= timestamp:
+                return v
+        
         
