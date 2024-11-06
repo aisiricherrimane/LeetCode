8aaa8d0964dd99f0d48ae9bb7d289373c3c7b712
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-06 08:48:35 -0500

    LC-lru-cache [Runtime: N/A; Memory: N/A]

--- linked-list/lru-cache.py
+++ linked-list/lru-cache.py
@@ -1,9 +1,10 @@
 class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
+    def __init__(self, key, value):
+        self.value = value
+        self.key = value
         self.next = None
         self.prev = None
+        
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
@@ -13,30 +14,32 @@ class LRUCache:
         self.left.next, self.right.prev = self.right, self.left
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if not key in self.cache:
             return -1
         self.remove(self.cache[key])
         self.insert(self.cache[key])
-        return self.cache[key].val
-    
+        return self.cache[key].value
+
+    def insert(self, node):
+        p = self.right.prev
+        n = self.right
+        p.next, node.prev = node, p
+        node.next, n.prev = n, node
+
     def remove(self, node):
         p = node.prev
         n = node.next
         p.next, n.prev = n, p
-    def insert(self, node):
-        p = self.right.prev
-        n = self.right
-        p.next = node
-        n.prev = node
-        node.prev = p
-        node.next = n
+
+
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
-        if len(self.cache) > self.capacity:
+        while len(self.cache) > self.capacity:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
