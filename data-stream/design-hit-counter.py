9076a0d0422b09276c92b651da38c1d867c9ac33
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-11-07 09:48:00 -0500

    LC-design-hit-counter [Runtime: 1 ms; Memory: 16.8 MB]

--- data-stream/design-hit-counter.py
+++ data-stream/design-hit-counter.py
@@ -2,16 +2,15 @@ class HitCounter:
 
     def __init__(self):
         self.store = deque()
-        self.l = 0
+
     def hit(self, timestamp: int) -> None:
         self.store.append(timestamp)
-        self.l += 1
+        
     def getHits(self, timestamp: int) -> int:
         while self.store and timestamp - self.store[0] >= 300:
             self.store.popleft()
-            self.l -= 1
-        return self.l 
-        
+        return len(self.store)
+
 
 
 # Your HitCounter object will be instantiated and called as such:
