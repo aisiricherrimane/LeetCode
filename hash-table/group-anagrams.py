37d7bf945a5020a69747676a5440e75d4b3f37c4
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-02 17:30:50 -0500

    LC-group-anagrams [Runtime: 24 ms; Memory: 22.1 MB]

--- hash-table/group-anagrams.py
+++ hash-table/group-anagrams.py
@@ -4,9 +4,7 @@ class Solution:
         for word in strs:
             count = [0] * 26
             for w in word:
-                count[ord(w) - ord('a')] += 1
+                count[ord(w) - ord('a')] = 1 + count[ord(w) - ord('a')]
             temp[tuple(count)].append(word)
-
-        return list(temp.values())
-
         
+        return list(temp.values())
