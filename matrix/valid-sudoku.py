66c81ce5b16f69572ed34c1218751c2b66d6852b
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-02 18:10:57 -0500

    LC-valid-sudoku [Runtime: 7 ms; Memory: 17.1 MB]

--- matrix/valid-sudoku.py
+++ matrix/valid-sudoku.py
@@ -1,18 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        square = collections.defaultdict(set)
 
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if board[r][c] == ".":
+                if board[r][c] == '.':
                     continue
-                if (board[r][c] in rows[r]) or (board[r][c] in cols[c]) or board[r][c] in squares[(r//3),(c//3)]:
-                   return False
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
-                squares[(r//3),(c//3)].add(board[r][c])
+                val = board[r][c]
+                if val not in rows[r] and val not in cols[c] and val not in square[(r//3, c//3)]:
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    square[(r//3, c//3)].add(val)
+                else:
+                    return False
         return True
+
+
 
         
