0a77d375ac5c4a7496b95247e17e5ed626b575be
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-03 18:44:47 -0500

    LC-longest-repeating-character-replacement [Runtime: 115 ms; Memory: 17.4 MB]

--- sliding-window/longest-repeating-character-replacement.py
+++ sliding-window/longest-repeating-character-replacement.py
@@ -1,20 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        maxf = 0
+        count = {}
         res = 0
         l = 0
-        maxf = 0
-        temp = {}
         for r in range(len(s)):
-            temp[s[r]] = 1 + temp.get(s[r], 0)
-            maxf = max(maxf, temp[s[r]])
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
 
             while (r - l + 1) - maxf > k:
-                temp[s[l]] -= 1
+                count[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1)
         return res
-
             
 
-
-        
