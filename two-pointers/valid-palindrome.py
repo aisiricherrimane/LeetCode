393a194de496df8c767c7a49eb422d54308fbe2a
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-03 12:33:44 -0500

    LC-valid-palindrome [Runtime: 15 ms; Memory: 17.4 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,19 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def alphanum(w):
-            if ord('a') <= ord(w) <= ord('z') or ord('A') <= ord(w) <= ord('Z') or ord('0') <= ord(w) <= ord('9'):
+        def check(c):
+            if ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z') or ord('0') <= ord(c) <= ord('9'):
                 return True
             else:
                 return False
-            
         
-        l = 0 
+        l = 0
         r = len(s) - 1
-
-        while l < r:
-            while l < r and not alphanum(s[l]):
+        while l <= r:
+            while l < r and not check(s[l]):
                 l += 1
-            while l < r and not alphanum(s[r]):
+            while l < r and not check(s[r]):
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
