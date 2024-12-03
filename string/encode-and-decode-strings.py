22929ed94de7ef5388eb8d626b6347784c8b365f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-02 21:00:45 -0500

    LC-encode-and-decode-strings [Runtime: 64 ms; Memory: 17.6 MB]

--- string/encode-and-decode-strings.py
+++ string/encode-and-decode-strings.py
@@ -3,31 +3,23 @@ class Codec:
         """Encodes a list of strings to a single string."""
         res = ''
         for word in strs:
-            res += '#' + str(len(word)) + word
+            res += str(len(word))+ '#' + word
         return res      
         
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings."""
         res = []
         i = 0
+
         while i < len(s):
-            # Find the start of the length prefix
-            while s[i] != '#':
-                i += 1
-            j = i + 1  # Start reading the length
-            
-            # Find where the length ends
-            while j < len(s) and s[j].isdigit():
+            j = i
+            while s[j] != '#':
                 j += 1
-            
-            # Extract the length
-            length = int(s[i + 1:j])
-            
-            # Move to the start of the word and extract it
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
             i = j
-            res.append(s[i:i + length])
-            
-            # Move past the extracted word
-            i += length
-        
         return res
+
+
