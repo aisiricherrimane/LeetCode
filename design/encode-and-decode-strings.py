ca4ee77f8faa3a49acc02321fee7d78de4263e46
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-02 20:56:37 -0500

    LC-encode-and-decode-strings [Runtime: N/A; Memory: N/A]

--- design/encode-and-decode-strings.py
+++ design/encode-and-decode-strings.py
@@ -1,42 +1,33 @@
 class Codec:
     def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
+        """Encodes a list of strings to a single string."""
         res = ''
         for word in strs:
             res += '#' + str(len(word)) + word
         return res      
         
     def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
-        """
-        res = [ ]
+        """Decodes a single string to a list of strings."""
+        res = []
         i = 0
         while i < len(s):
+            # Find the start of the length prefix
             while s[i] != '#':
                 i += 1
-            j = i + 1
+            j = i + 1  # Start reading the length
+            
+            # Find where the length ends
             while j < len(s) and s[j].isdigit():
                 j += 1
-            i = i + 1
-            length = int(s[i:j])
-
-            i = j 
+            
+            # Extract the length
+            length = int(s[i + 1:j])
+            
+            # Move to the start of the word and extract it
+            i = j
             res.append(s[i:i + length])
-
-            i = i + length
+            
+            # Move past the extracted word
+            i += length
+        
         return res
-
-
-
-
-
-        
-
-        
-        
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
