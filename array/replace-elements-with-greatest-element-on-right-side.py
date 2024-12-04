156bc5d65ffad6574fca5ad1291d1d36a0a3d206
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-03 20:56:59 -0500

    LC-replace-elements-with-greatest-element-on-right-side [Runtime: 34 ms; Memory: 18.4 MB]

---
+++ array/replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,9 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        max_element = -1
+        for i in range(len(arr) - 1, -1, -1):
+            temp = arr[i]
+            arr[i] = max_element
+            max_element = max(max_element, temp)
+        return arr
+        
