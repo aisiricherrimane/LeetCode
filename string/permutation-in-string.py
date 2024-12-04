a7eab333139088438325396de5b029c9f63599d8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-03 20:30:29 -0500

    LC-permutation-in-string [Runtime: 47 ms; Memory: 17.5 MB]

--- string/permutation-in-string.py
+++ string/permutation-in-string.py
@@ -1,27 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        l_s1 = len(s1)
+        l_s2 = len(s2)
         count_s1 = Counter(s1)
-        l1 = len(s1)
-        l2 = len(s2)
-        window = Counter(s2[:l1])
+        window = Counter(s2[:l_s1])
 
-        if l1 > l2:
-            return False
-
-        if window == count_s1:
+        if count_s1 == window:
             return True
-
-        for i in range(l1, l2):
+        
+        for i in range(l_s1, l_s2):
+            window[s2[i - l_s1]] -= 1
             window[s2[i]] += 1
-            window[s2[i - l1]] -= 1
 
-            if window[s2[i - l1]] == 0:
-                del window[s2[i - l1]]
-            
             if window == count_s1:
                 return True
         return False
-
-
-       
-        
