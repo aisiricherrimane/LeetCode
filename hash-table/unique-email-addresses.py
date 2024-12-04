9a9121768bab0dbd13c3a128b8303481a150d0b3
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-04 09:30:59 -0500

    LC-unique-email-addresses [Runtime: 3 ms; Memory: 17.6 MB]

--- hash-table/unique-email-addresses.py
+++ hash-table/unique-email-addresses.py
@@ -3,10 +3,8 @@ class Solution:
         unique_emails = set()
     
         for email in emails:
-            local, domain = email.split('@')  # Split into local and domain parts
-            # Process the local name: remove '.' and ignore everything after '+'
-            local = local.split('+')[0].replace('.', '')
-            # Combine the processed local name with the domain
+            local, domain = email.split('@') 
+            local = local.split('+')[0].replace('.', '') 
             unique_emails.add(local + '@' + domain)
         
         return len(unique_emails)
