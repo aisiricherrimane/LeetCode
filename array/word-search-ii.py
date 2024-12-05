2feffb7be67e13163496c5bb9f0fd48aa8028613
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-04 22:45:34 -0500

    LC-word-search-ii [Runtime: 425 ms; Memory: 18.9 MB]

--- array/word-search-ii.py
+++ array/word-search-ii.py
@@ -1,44 +1,47 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def buildtree(words):
-            def trieNode():
-                return defaultdict(trieNode)
-            root = trieNode()
+        def buildTree(words):
+            def TrieNode():
+                return defaultdict(TrieNode)
+            
+            root = TrieNode()
             for word in words:
                 node = root
                 for letter in word:
                     node = node[letter]
                 node['#'] = word
             return root
-        Trie = buildtree(words)
-        rows, cols = len(board), len(board[0])
-        res = [ ]
+        
+        Trie = buildTree(words)
+        rows = len(board)
+        cols = len(board[0])
+        res = []
 
-        def dfs(r, c, section):
+        def dfs(r, c, node):
             letter = board[r][c]
-
-            if letter not in section:
-                return 
             
-            next_node = section[letter]
+            if letter not in node:
+                return 
+            next_node = node[letter]
 
             if '#' in next_node:
                 res.append(next_node['#'])
-                del next_node["#"] 
+                del next_node['#']
+            
             board[r][c] = ''
-
-            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                R, C = r + dr, c + dc
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                R, C = dr + r, dc + c
                 if 0 <= R < rows and 0 <= C < cols and board[R][C] in next_node:
                     dfs(R, C, next_node)
-
             board[r][c] = letter
 
             if not next_node:
-                section.pop(letter)
-        
+                node.pop(letter)
+
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] in Trie:
                     dfs(r, c, Trie)
         return res
+
+         
