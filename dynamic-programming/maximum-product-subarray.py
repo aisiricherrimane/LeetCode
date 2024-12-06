2e870fab0d4ee641c50b4f7da8cbc76908c5c6e1
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-06 10:25:55 -0500

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/maximum-product-subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        currMin = 1
+        currMax = 1
+
+        for n in nums:
+            if n == 0:
+                currMin = 1
+                currMax = 1
+            temp = currMax * n
+            currMax = max(currMin * n, currMax * n, n)
+            currMin = max(temp, currMin * n, n)
+            res = max(res, currMax, currMin)
+        return res
+
+
+
+        
