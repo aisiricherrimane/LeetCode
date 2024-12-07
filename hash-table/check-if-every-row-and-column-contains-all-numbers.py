e97f2f6848757f5f507256eb17de0e85ea9380c0
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-07 18:41:34 -0500

    LC-check-if-every-row-and-column-contains-all-numbers [Runtime: N/A; Memory: N/A]

--- hash-table/check-if-every-row-and-column-contains-all-numbers.py
+++ hash-table/check-if-every-row-and-column-contains-all-numbers.py
@@ -1,15 +1,13 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         check = set(i for i in range(1, len(matrix) + 1))
-        n = len(matrix)
-
-        for r in range(n):
-            row_set = set()
-            col_set = set()
-            for c in range(n):
-                row_set.add(matrix[r][c])
-                col_set.add(matrix[c][r])
-            if row_set != check or col_set != check:
+        for r in range(len(matrix)):
+            rows = set()
+            cols = set()
+            for c in range(len(matrix[0])):
+                rows.add(matrix[r][c])
+                cols.add(matrix[c][r])
+            if rows != check and cols != check:
                 return False
         return True
                 
