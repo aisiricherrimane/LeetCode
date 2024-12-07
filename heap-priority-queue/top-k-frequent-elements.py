cec4c06db2925805af393aea67b8ff2f57f60099
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-07 17:24:11 -0500

    LC-top-k-frequent-elements [Runtime: 11 ms; Memory: 23.7 MB]

--- heap-priority-queue/top-k-frequent-elements.py
+++ heap-priority-queue/top-k-frequent-elements.py
@@ -1,21 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = [[] for _ in range(len(nums) + 1)]
-        count_dict = {}
-        for n in nums:
-            count_dict[n] = 1 + count_dict.get(n, 0)
+        count = [[] for i in range(len(nums) + 1)]
+        temp = {}
+        for num in nums:
+            temp[num] = 1 + temp.get(num, 0)
         
-        for n, c in count_dict.items():
-            freq[c].append(n)
+        for n, c in temp.items():
+            count[c].append(n)
+        
         res = []
-        for nos in range(len(freq) - 1, -1, -1):
-            for n in freq[nos]:
+        for i in range(len(count) - 1, -1, -1):
+            for n in count[i]:
                 res.append(n)
                 k -= 1
                 if k == 0:
                     return res
-
-
-
-
         
