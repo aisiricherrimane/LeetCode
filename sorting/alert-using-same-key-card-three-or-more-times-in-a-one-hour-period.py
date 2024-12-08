3e9fe3b5fe06c52ea2a36c620b95df18269e67f2
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-08 10:09:45 -0500

    LC-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period [Runtime: 119 ms; Memory: 40 MB]

--- sorting/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
+++ sorting/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -18,7 +18,7 @@ class Solution:
                 if times[i + 2] - times[i] <= 60:
                     res.append(person)
                     break
-        return res
+        return sorted(res)
 
             
         
