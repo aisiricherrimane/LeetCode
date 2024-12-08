fbde053d3f064039eab1bffcd4ac6d4d9f44494d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-08 10:46:57 -0500

    LC-subdomain-visit-count [Runtime: 3 ms; Memory: 17.4 MB]

--- counting/subdomain-visit-count.py
+++ counting/subdomain-visit-count.py
@@ -1,16 +1,14 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        temp = defaultdict(int)
+        store = defaultdict(int)
 
-        for domain in cpdomains:
-            count, dom = domain.split(' ')
+        for temp in cpdomains:
+            count, dom = temp.split(' ')
             count = int(count)
             frags = dom.split('.')
             for i in range(len(frags)):
-                temp['.'.join(frags[i:])] += count
+                store['.'.join(frags[i:])] += count
         ans = []
-        for dom, cnt in temp.items():
+        for dom, cnt in store.items():
             ans.append('{} {}'.format(cnt, dom))
         return ans
-
-
