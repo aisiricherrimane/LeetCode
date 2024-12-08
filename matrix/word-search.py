288f88404197e2237e3bbc3deac5a76b3c1f512e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-08 09:43:53 -0500

    LC-word-search [Runtime: 3129 ms; Memory: 17.5 MB]

--- matrix/word-search.py
+++ matrix/word-search.py
@@ -1,27 +1,24 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board or not word:
-            return False
         rows = len(board)
         cols = len(board[0])
 
         def dfs(r, c, ind):
-            if ind == len(word):
+            if ind >= len(word):
                 return True
             if r < 0 or r == rows or c < 0 or c == cols or board[r][c] != word[ind]:
                 return False
             letter = board[r][c]
-            board[r][c] = ''
+            board[r][c] = ' '
             found = dfs(r + 1, c, ind + 1) or dfs(r - 1, c, ind + 1) or dfs(r, c + 1, ind + 1) or dfs(r, c - 1, ind + 1)
             board[r][c] = letter
             return found
-                    
 
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == word[0]:
-                    if dfs(r, c, 0):
-                        return True
+                board[r][c] == word[0]
+                if dfs(r, c, 0):
+                    return True
         return False
 
         
