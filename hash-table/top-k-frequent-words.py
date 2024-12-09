57e4d0451f87c4649bc4a5ed31340dfbf2974ccc
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-09 14:59:27 -0500

    LC-top-k-frequent-words [Runtime: 0 ms; Memory: 17.5 MB]

--- hash-table/top-k-frequent-words.py
+++ hash-table/top-k-frequent-words.py
@@ -1,15 +1,14 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         cnt = Counter(words)
-       
         heap = []
-        for word, freq in cnt.items():
-            heapq.heappush(heap, (-freq, word))
+        for word, c in cnt.items():
+            heapq.heappush(heap, [-1 * c, word])
         res = []
-        while k:
-            res.append(heappop(heap)[1])
+        while k > 0:
+            res.append(heapq.heappop(heap)[1])
             k -= 1
-        return res
 
+        return res
         
         
