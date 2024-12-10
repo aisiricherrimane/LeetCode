034388bee3841d4a13e4b838a20423a936c14e75
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-10 17:27:59 -0500

    LC-word-search-ii [Runtime: 399 ms; Memory: 18.6 MB]

--- matrix/word-search-ii.py
+++ matrix/word-search-ii.py
@@ -1,47 +1,40 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        def buildTree(words):
-            def TrieNode():
-                return defaultdict(TrieNode)
-            
-            root = TrieNode()
+        def buildTrie(words):
+            root = {}
             for word in words:
                 node = root
-                for letter in word:
-                    node = node[letter]
-                node['#'] = word
+                for char in word:  # Iterate through characters of the word
+                    node = node.setdefault(char, {})
+                node['#'] = word  # Mark the end of a word
             return root
-        
-        Trie = buildTree(words)
-        rows = len(board)
-        cols = len(board[0])
+
+        Trie = buildTrie(words)
+        rows, cols = len(board), len(board[0])
         res = []
 
         def dfs(r, c, node):
             letter = board[r][c]
+           
             
-            if letter not in node:
-                return 
             next_node = node[letter]
-
-            if '#' in next_node:
+            if '#' in next_node:  # Word found
                 res.append(next_node['#'])
-                del next_node['#']
+                del next_node['#']  # Avoid duplicates
             
-            board[r][c] = ''
+            board[r][c] = ''  # Mark as visited
             for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                R, C = dr + r, dc + c
-                if 0 <= R < rows and 0 <= C < cols and board[R][C] in next_node:
-                    dfs(R, C, next_node)
-            board[r][c] = letter
-
-            if not next_node:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] in next_node:
+                    dfs(nr, nc, next_node)
+            board[r][c] = letter  # Restore original value
+            
+            if not next_node:  # Clean up Trie node
                 node.pop(letter)
 
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] in Trie:
                     dfs(r, c, Trie)
-        return res
 
-         
+        return res
