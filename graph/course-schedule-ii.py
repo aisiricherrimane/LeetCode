062818dfdb768a9636148fc20df35cbc5ae7fa51
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-10 16:45:44 -0500

    LC-course-schedule-ii [Runtime: N/A; Memory: N/A]

--- graph/course-schedule-ii.py
+++ graph/course-schedule-ii.py
@@ -1,31 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        course_map = {i:[] for i in range(numCourses)}
+        adj = {i:[ ] for i in range(numCourses)}
 
-        for c, p in prerequisites:
-            course_map[c].append(p)
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
         
-        
-        visit = set()
+        cycle = set()
         visited = set()
         output = []
         def dfs(crs):
+            if crs in cycle:
+                return False
             if crs in visited:
                 return True
-            if crs in visit:
-                return False
-            visit.add(crs)
-            for pre in course_map[crs]:
+            cycle.add(crs)
+            for pre in adj[crs]:
                 if not dfs(pre):
                     return False
-            visit.remove(crs)
+            cycle.remove(crs)
             visited.add(crs)
             output.append(crs)
             return True
-
-        for i in range(numCourses):
-            if not dfs(i):
-                return []
-        return output 
-
         
+        for c in range(numCourses):
+            if not dfs(c): return False
+        
+        return output
+
+            
