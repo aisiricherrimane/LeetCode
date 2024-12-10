d6100e541afa75553cfb6022f118477daa2e7ba6
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-10 12:14:09 -0500

    LC-pacific-atlantic-water-flow [Runtime: 27 ms; Memory: 19.2 MB]

--- breadth-first-search/pacific-atlantic-water-flow.py
+++ breadth-first-search/pacific-atlantic-water-flow.py
@@ -5,17 +5,18 @@ class Solution:
         rows = len(heights)
         cols = len(heights[0])
 
-        def dfs(r, c, group, prevH):
-            if r >= 0 and r < rows and c >= 0 and c < cols and (r, c) not in group and heights[r][c] >= prevH:
-                group.add((r, c))
-                dfs(r + 1, c, group, heights[r][c])
-                dfs(r - 1, c, group, heights[r][c])
-                dfs(r, c - 1, group, heights[r][c])
-                dfs(r, c + 1, group, heights[r][c])
-        
+        def dfs(r, c, visit, preV):
+            if 0 <= r < rows and 0 <= c < cols and (r, c) not in visit and heights[r][c] >= preV:
+                visit.add((r, c))
+                dfs(r + 1, c, visit, heights[r][c])
+                dfs(r - 1, c, visit, heights[r][c])
+                dfs(r, c + 1, visit, heights[r][c])
+                dfs(r, c - 1, visit, heights[r][c])
+
         for r in range(rows):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, cols - 1, atl, heights[r][cols - 1])
+        
         for c in range(cols):
             dfs(0, c, pac, heights[0][c])
             dfs(rows - 1, c, atl, heights[rows - 1][c])
@@ -25,4 +26,3 @@ class Solution:
                 if (r, c) in pac and (r, c) in atl:
                     res.append((r, c))
         return res
-        
