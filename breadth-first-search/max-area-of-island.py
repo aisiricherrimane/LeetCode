b2e03788448f69a1a364cb70c2768345da0c5252
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-09 22:58:41 -0500

    LC-max-area-of-island [Runtime: 23 ms; Memory: 18 MB]

--- breadth-first-search/max-area-of-island.py
+++ breadth-first-search/max-area-of-island.py
@@ -1,21 +1,17 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
         area = 0
- 
+
         def dfs(r, c):
-            if r < 0 or r == row or c < 0 or c == col or grid[r][c] != 1:
+            if r < 0 or r == rows or c < 0. or c == cols or grid[r][c] == 0:
                 return 0
-            grid[r][c] = ''
-           
+            grid[r][c] = 0
             return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
-
-
-        for r in range(row):
-            for c in range(col):
+            
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 1:
                     area = max(area, dfs(r, c))
-
-        return area
-        
+        return area        
