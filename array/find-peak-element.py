46d63c00fdba54dc8b2b0dd0cf7b5fb063bfa711
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-10 19:29:39 -0500

    LC-find-peak-element [Runtime: 0 ms; Memory: 17.4 MB]

--- array/find-peak-element.py
+++ array/find-peak-element.py
@@ -2,15 +2,13 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         l = 0 
         r = len(nums) - 1
-        while l < r:
-            mid = (l + r) // 2
 
-            if nums[mid - 1] <= nums[mid] >= nums[mid + 1]:
-                return mid
-            elif nums[mid] < nums[mid + 1]:
-                l = mid + 1
+        while l <= r:
+            m = (l + r) // 2
+            if m > 0 and nums[m] < nums[m - 1]:
+                r = m - 1
+            elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
+                l = m + 1
             else:
-                r = mid - 1
-        return l
-
+                return m
         
