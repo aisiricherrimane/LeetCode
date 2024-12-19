dcd066422db79fb9df0e14efc3f5e14a2d463ba8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-19 14:46:09 -0500

    LC-valid-anagram [Runtime: 15 ms; Memory: 17.9 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -1,12 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        countS = {}
-        countT = {}
+        s_count = {}
+        t_count = {}
 
         if len(s) != len(t):
             return False
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT 
+            s_count[s[i]] = 1 + s_count.get(s[i], 0)
+            t_count[t[i]] = 1 + t_count.get(t[i], 0)
+        
+        return s_count == t_count
