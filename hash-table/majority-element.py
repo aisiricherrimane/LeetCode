5b649a75bb053085fcd4a38ca3acd677b348876c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-19 19:58:17 -0500

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- hash-table/majority-element.py
+++ hash-table/majority-element.py
@@ -1,14 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
+       nums.sort()
 
-        l = 0
-        for r in range(len(nums)):
-            while nums[r] != nums[l]:
-                l += 1
-            if r - l + 1 > len(nums) // 2:
-                return nums[r]
-        return -1
-
-
-        
+       l = 0 
+       r = len(nums) - 1
+       mid = r + l // 2
+       return nums[mid]
