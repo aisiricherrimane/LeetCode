e5add53dfba35bd1455fce81197d552d9c43c871
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-19 20:07:16 -0500

    LC-majority-element [Runtime: 0 ms; Memory: 19.4 MB]

--- array/majority-element.py
+++ array/majority-element.py
@@ -1,8 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-       nums.sort()
-
-       l = 0 
-       r = len(nums) - 1
-       mid = r + l // 2
-       return nums[mid]
+        nums.sort()
+        return nums[len(nums) // 2]
