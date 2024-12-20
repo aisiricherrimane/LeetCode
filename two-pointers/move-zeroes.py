5337a7d028be530954b8dd75c10c0f61d0ebc3c9
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2024-12-19 19:37:52 -0500

    LC-move-zeroes [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/move-zeroes.py
@@ -0,0 +1,22 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < 2:
+            return nums
+        l = 0
+        r = l + 1
+
+        while r < len(nums):
+            while nums[l] != 0:
+                l += 1
+            while nums[r] == 0:
+                r += 1
+            temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
+            l += 1
+            r += 1
+        return nums
+        
