82fa46b4140851473a4bbb130603cd1494305b9f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-03 01:32:57 -0500

    LC-identify-the-largest-outlier-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ counting/identify-the-largest-outlier-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getLargestOutlier(self, nums: List[int]) -> int:
+        mean = sum(nums) / len(nums)
+        ans = 0
+        res = nums[0]
+        for n in nums:
+            if abs(mean - n) > ans:
+                ans = mean - n
+                res = n
+        return res
+        
