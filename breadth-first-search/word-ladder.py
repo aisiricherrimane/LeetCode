95567288f0d203214221fe99c846db116f002b87
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-04 06:12:45 -0500

    LC-word-ladder [Runtime: N/A; Memory: N/A]

--- breadth-first-search/word-ladder.py
+++ breadth-first-search/word-ladder.py
@@ -1,38 +1,38 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
         if endWord not in wordList:
             return 0
-        if beginWord not in wordList:
-            wordList.append(beginWord)
+        
 
-        temp = defaultdict(list)
+        store = defaultdict(list)
 
         for word in wordList:
             for i in range(len(word)):
                 pattern = word[:i] + '*' + word[i + 1:]
-                temp[pattern].append(word)
+                store[pattern].append(word)
 
         q = deque()
         q.append(beginWord)
         visit = set()
-        visit.add(beginWord)
-        res = 1
+        visit.add(word)
+        length = 1
+
         while q:
             for _ in range(len(q)):
                 word = q.popleft()
 
                 if word == endWord:
-                    return res
-
-                visit.add(word)
+                    return length
 
                 for i in range(len(word)):
                     pattern = word[:i] + '*' + word[i + 1:]
-                    for neiW in temp[pattern]:
-                        if neiW not in visit:
+                    for neiW in store[pattern]:
+                        if neiW not in q:
                             q.append(neiW)
-    
-            res += 1
-        return 0
+                            visit.add(word)
+            length += 1
+        
+                            
+        
 
+        
