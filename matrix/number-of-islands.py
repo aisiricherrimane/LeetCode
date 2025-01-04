821acf5f69e44aef6ac36748b67e4e2497563071
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-04 09:40:27 -0500

    LC-number-of-islands [Runtime: 246 ms; Memory: 20.1 MB]

--- matrix/number-of-islands.py
+++ matrix/number-of-islands.py
@@ -1,16 +1,17 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
         rows = len(grid)
         cols = len(grid[0])
+        islands = 0 
 
         def dfs(r, c):
-            grid[r][c] = ''
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                R, C = dr + r, dc + c
-                if 0 <= R < rows and 0 <= C < cols and grid[R][C] == '1':
-                    dfs(R, C)
-            return 
+            if r >= 0 and r < rows and c >= 0 and c < cols and grid[r][c] == '1':
+                grid[r][c] = '0'
+                dfs(r + 1, c)
+                dfs(r - 1, c)
+                dfs(r, c + 1)
+                dfs(r, c - 1)
+            
 
         for r in range(rows):
             for c in range(cols):
@@ -18,3 +19,4 @@ class Solution:
                     dfs(r, c)
                     islands += 1
         return islands
+        
