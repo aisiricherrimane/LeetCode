27ff4e2ed8eccb05a80f35cce8cbfc0750bb3edf
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-04 05:42:13 -0500

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 17.7 MB]

--- sorting/merge-sorted-array.py
+++ sorting/merge-sorted-array.py
@@ -3,22 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        r = n + m - 1
+        curr = m + n - 1
 
-        while m>0 and n>0:
-            if nums1[m-1]>nums2[n-1]:
-                nums1[r] = nums1[m-1]
-                m-=1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[curr] = nums1[m - 1]
+                m -= 1
             else:
-                nums1[r] = nums2[n-1]
-                n-=1
-            r-=1
-
-        while n>0:
-            nums1[r] = nums2[n-1]
-            r-=1
-            n-=1
-
-
-
-            
+                nums1[curr] = nums2[n - 1]
+                n -= 1
+            curr -= 1
+        
+        while n > 0:
+            nums1[curr] = nums2[n - 1]
+            n -= 1
+            curr -= 1
