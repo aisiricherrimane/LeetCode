762ea41f13c8526cb636f03a60dd35ab9105d658
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-05 00:44:31 -0500

    LC-merge-k-sorted-lists [Runtime: 50 ms; Memory: 21.1 MB]

--- divide-and-conquer/merge-k-sorted-lists.py
+++ divide-and-conquer/merge-k-sorted-lists.py
@@ -5,35 +5,38 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
+        if not lists or len(lists) == 0:
             return None
 
         while len(lists) > 1:
-            merged = []
+            mergedlist = []
             for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                merged.append(self.mergeTwoLists(l1, l2))
-            lists = merged
+                list1 = lists[i]
+                list2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedlist.append(self.mergetwo(list1, list2))
+            lists = mergedlist
         return lists[0]
-
     
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergetwo(self, l1, l2):
         dummy = ListNode()
         curr = dummy
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next= list1
-                list1 = list1.next
+        while l1 and l2:
+            if l1.val > l2.val:
+                curr.next = ListNode(l2.val)
+                l2 = l2.next
             else:
-                curr.next = list2
-                list2 = list2.next
+                curr.next = ListNode(l1.val)
+                l1 = l1.next
             curr = curr.next
-            
-        if list1:
-            curr.next = list1
-        if list2:
-            curr.next= list2
+        
+        if l1:
+            curr.next = l1
+        else:
+            curr.next = l2
         return dummy.next
+
+
+        
+
         
