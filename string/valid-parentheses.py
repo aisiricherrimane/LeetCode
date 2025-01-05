ba532d241198aaadc7ff971b0ece185add11ae92
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-05 00:28:22 -0500

    LC-valid-parentheses [Runtime: 0 ms; Memory: 18 MB]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -1,14 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        check = {'}' : '{', ']' : '[', ')' : '('}
         stack = []
+        check = { ']' : '[', ")" : '(', '}' : '{'}
 
-        for i in s:
-            if i in check:
-                if stack and stack[-1] == check[i]:
+        for parenthesis in s:
+            if parenthesis in check:
+                if stack and stack[-1] == check[parenthesis]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(i)
+                stack.append(parenthesis)
         return True if not stack else False
