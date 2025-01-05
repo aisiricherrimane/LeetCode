a0036d67c90e1f0a73ec1abbb8cfda1841457fc3
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-04 22:54:02 -0500

    LC-top-k-frequent-elements [Runtime: 11 ms; Memory: 24.3 MB]

--- bucket-sort/top-k-frequent-elements.py
+++ bucket-sort/top-k-frequent-elements.py
@@ -1,18 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = [[] for i in range(len(nums) + 1)]
-        temp = {}
-        for num in nums:
-            temp[num] = 1 + temp.get(num, 0)
-        
-        for n, c in temp.items():
-            count[c].append(n)
+        count = Counter(nums)
+        freq = [[] for i in range(len(nums) + 1)]
+        for num, c in count.items():
+            freq[c].append(num)
         
         res = []
-        for i in range(len(count) - 1, -1, -1):
-            for n in count[i]:
-                res.append(n)
+        for i in range(len(freq) - 1, -1, -1):
+            for char in freq[i]:
+                res.append(char)
                 k -= 1
                 if k == 0:
                     return res
-        
+
+      
