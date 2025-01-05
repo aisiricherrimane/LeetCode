35fc03fb2db944a10ed744fccacaab8b2cd6028e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-04 21:52:37 -0500

    LC-reorganize-string [Runtime: N/A; Memory: N/A]

---
+++ sorting/reorganize-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        char_count = Counter(s)
+        max_count, letter = 0, ''
+
+        for char, count in char_count.items():
+            if count > max_count:
+                max_count = count
+                letter = char
+        
+        if max_count > (len(s) + 1) // 2:
+            return ''
+        
+        ans = [''] * len(s)
+        index = 0
+
+        while char_count[letter] != 0:
+            ans[index] = letter
+            index += 2
+            char_count[letter] -= 1
+        
+        for char, count in char_count.items():
+            while count > 0:
+                if index > len(s):
+                    index = 1
+                ans[index] = char
+                index += 2
+                count -= 1
+        return ''.join(ans)
+
+
+
+        
+        
+        
