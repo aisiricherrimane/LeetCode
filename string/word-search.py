4ae360a50fdc584fcf65fe74b92475df6051915a
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-06 04:13:26 -0500

    LC-word-search [Runtime: 3225 ms; Memory: 18.1 MB]

--- string/word-search.py
+++ string/word-search.py
@@ -7,18 +7,16 @@ class Solution:
             if ind >= len(word):
                 return True
             if r < 0 or r == rows or c < 0 or c == cols or board[r][c] != word[ind]:
-                return False
+                return 
             letter = board[r][c]
-            board[r][c] = ' '
+            board[r][c] = ''
             found = dfs(r + 1, c, ind + 1) or dfs(r - 1, c, ind + 1) or dfs(r, c + 1, ind + 1) or dfs(r, c - 1, ind + 1)
             board[r][c] = letter
             return found
 
         for r in range(rows):
             for c in range(cols):
-                board[r][c] == word[0]
-                if dfs(r, c, 0):
-                    return True
+                if board[r][c] == word[0]:
+                    if dfs(r, c, 0): return True
         return False
-
         
