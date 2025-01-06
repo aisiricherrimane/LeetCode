a410ce1c2a243740c1d3cd1a2606e0efbedd6129
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-06 02:30:09 -0500

    LC-analyze-user-website-visit-pattern [Runtime: 7 ms; Memory: 19.1 MB]

--- array/analyze-user-website-visit-pattern.py
+++ array/analyze-user-website-visit-pattern.py
@@ -1,21 +1,23 @@
+from collections import defaultdict
+from itertools import combinations
+
 class Solution:
-    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+    def mostVisitedPattern(self, username, timestamp, website):
         data = sorted(zip(timestamp, username, website))
-        user_web = defaultdict(list)
         
+        user_web = defaultdict(list)
         for _, user, web in data:
             user_web[user].append(web)
-        store = defaultdict(int)
+        
+        store = defaultdict(set)
         for user, webs in user_web.items():
             for pattern in set(combinations(webs, 3)):
-                store[tuple(pattern)] += 1
+                store[pattern].add(user)
         
-        res = []
-        maxcount = 0
+        maxcount, res = 0, []
         for pattern, users in store.items():
-            if users > maxcount:
-                maxcount = users
+            if len(users) > maxcount or (len(users) == maxcount and pattern < tuple(res)):
+                maxcount = len(users)
                 res = pattern
-        return list(res)
-
         
+        return list(res)
