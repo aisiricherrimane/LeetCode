4dfc402d2ddc05e7cb859f47cd3398fa6a68fc35
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-06 02:29:59 -0500

    LC-analyze-user-website-visit-pattern [Runtime: N/A; Memory: N/A]

--- array/analyze-user-website-visit-pattern.py
+++ array/analyze-user-website-visit-pattern.py
@@ -1,15 +1,21 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        data = sorted(zip(timestamp, username, website))
         user_web = defaultdict(list)
-        for i, user in enumerate(username):
-            user_web[user].append(website[i])
+        
+        for _, user, web in data:
+            user_web[user].append(web)
         store = defaultdict(int)
         for user, webs in user_web.items():
-            store[tuple(webs)] += 1
-        maxcount = 0
+            for pattern in set(combinations(webs, 3)):
+                store[tuple(pattern)] += 1
+        
         res = []
-        for key, count in sorted(store.items(), key=lambda x: x[0]):
-            if count > maxcount:
-                maxcount = count
-                res = list(key)
-        return res
+        maxcount = 0
+        for pattern, users in store.items():
+            if users > maxcount:
+                maxcount = users
+                res = pattern
+        return list(res)
+
+        
