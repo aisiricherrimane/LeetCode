981638ecc2da0dd3817dd4ab84a0ccc8f22e4003
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-06 01:10:12 -0500

    LC-basic-calculator [Runtime: N/A; Memory: N/A]

--- recursion/basic-calculator.py
+++ recursion/basic-calculator.py
@@ -1,53 +1,48 @@
 class Solution:
     def calculate(self, s: str) -> int:
         def evaluate(stack):
-            res = 0 if not stack else stack.pop()
-            while stack and stack[-1] != '(':
+            res = stack.pop() if stack else 0
+            while stack and stack[-1] != "(":
                 sign = stack.pop()
-                if sign == '+':
+                if sign == "+":
                     res += stack.pop()
-                elif sign == '-':
+                elif sign == "-":
                     res -= stack.pop()
             return res
         
         stack = []
-        num = 0
-        sign = '+'
+        num, sign = 0, "+"
         i = 0
 
         while i < len(s):
             char = s[i]
+            
             if char.isdigit():
-                num = num*10 + int(char)
-            elif char in '+-':
-                if sign == '+':
+                num = num * 10 + int(char)  # Build the current number
+            elif char in "+-":
+                if sign == "+":
+                    stack.append(num)  # Add the current number to the stack
+                elif sign == "-":
+                    stack.append(-num)  # Add the negative of the current number
+                num = 0  # Reset the current number
+                sign = char  # Update the sign
+            elif char == "(":
+                stack.append(sign)  # Save the current sign
+                stack.append("(")  # Mark the start of a sub-expression
+                sign = "+"  # Reset sign for the sub-expression
+            elif char == ")":
+                if sign == "+":
                     stack.append(num)
-                elif sign == '-':
+                elif sign == "-":
                     stack.append(-num)
-                num = 0
-                sign = char
-            elif char == '(':
-                stack.append(sign)
-                stack.append("(")
-                sign = '+'
-            elif char == ')':
-                if sign == '+':
-                    stack.append(num)
-                elif sign == '-':
-                    stack.append(-num)
-                num = evaluate(stack)
-                
+                num = evaluate(stack)  # Solve the sub-expression
+                stack.pop()  # Remove the "(" from the stack
             i += 1
         
-        if sign == '+':
+        # Add the last number
+        if sign == "+":
             stack.append(num)
-        elif sign == '-':
+        elif sign == "-":
             stack.append(-num)
-        
-        return evaluate(stack)
 
-
-
-
-                    
-        
+        return evaluate(stack)  # Evaluate whatever is left in the stack
