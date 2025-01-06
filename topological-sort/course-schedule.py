0f67d31b71bed7ce12bd2916305fc0adbe9e2c6c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-06 01:19:13 -0500

    LC-course-schedule [Runtime: 7 ms; Memory: 19.5 MB]

--- topological-sort/course-schedule.py
+++ topological-sort/course-schedule.py
@@ -1,27 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = {i:[] for i in range(numCourses)}
-
-        for crs, pre in prerequisites:
-            adj[crs].append(pre)
-
-        visit = set()
+        for c, p in prerequisites:
+            adj[c].append(p)
         
+        visit = set()
+
         def dfs(crs):
             if crs in visit:
                 return False
             if adj[crs] == []:
                 return True
             visit.add(crs)
-            for p in adj[crs]:
-                if not dfs(p): return False
-            visit.remove(crs)
+            for neiC in adj[crs]:
+                if not dfs(neiC):return False
             adj[crs] = []
+            visit.remove(crs)
             return True
         
         for c in range(numCourses):
-            if not dfs(c): return False
+            if not dfs(c):
+                return False
         return True
-        
-        
-        
