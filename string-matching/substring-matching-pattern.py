8946f41a65174434b3b4461aba046c4580d31288
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-07 02:00:57 -0500

    LC-substring-matching-pattern [Runtime: 0 ms; Memory: 18 MB]

--- string-matching/substring-matching-pattern.py
+++ string-matching/substring-matching-pattern.py
@@ -6,7 +6,7 @@ class Solution:
         if left == -1:
             return False
         
-        right = s[left + len(p[:mid]) - 1:].find(p[mid + 1:])
+        right = s[left + len(p[:mid]):].find(p[mid + 1:])
         if right == -1:
             return False
         return True
