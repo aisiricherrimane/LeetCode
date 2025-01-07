d333727cb9a77bd016319276405064c0891e4162
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-07 02:16:21 -0500

    LC-palindrome-number [Runtime: 7 ms; Memory: 17.8 MB]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,12 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0 :
+            return False
         temp = x
-        y = 0
-
-        while temp > 0:
-            digit = temp % 10
-            y = y * 10 + digit
+        compare = 0
+        while temp:
+            compare = compare * 10 + temp % 10
             temp = temp // 10
-        return y == x
-
+        return compare == x
         
