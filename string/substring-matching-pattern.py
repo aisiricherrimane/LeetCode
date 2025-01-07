11d099a6d629e3ecf31b53ad307011b27195d21d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-07 01:58:06 -0500

    LC-substring-matching-pattern [Runtime: N/A; Memory: N/A]

--- string/substring-matching-pattern.py
+++ string/substring-matching-pattern.py
@@ -1,16 +1,13 @@
 class Solution:
     def hasMatch(self, s: str, p: str) -> bool:
-        # Split the pattern into prefix and suffix
-        temp = p.split('*')
-        pre = temp[0]
-        suf = temp[1]
+        mid = p.index('*')
+
+        left = s.find(p[:mid])
+        if left == -1:
+            return False
         
-        i = 0
-        while i <= len(s) - len(pre):  # Ensure `pre` fits into the substring
-            # Check if `pre` matches the current substring
-            if pre == s[i:i+len(pre)]:
-                # Check if `suf` is in the remaining substring after `pre`
-                if suf == "" or s[i+len(pre):].startswith(suf):
-                    return True
-            i += 1  # Increment `i` to avoid infinite loop
-        return False
+        right = s[left + len(p[:mid]) - 1:].find(p[mid + 1:])
+        if right == -1:
+            return False
+        return True
+
