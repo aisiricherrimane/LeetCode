03e2046f5bff63df1ae6232bc846cdc6dd2138cf
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-07 06:05:32 -0500

    LC-sliding-window-maximum [Runtime: 141 ms; Memory: 31.9 MB]

--- queue/sliding-window-maximum.py
+++ queue/sliding-window-maximum.py
@@ -7,15 +7,17 @@ class Solution:
             while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
+
         res.append(nums[dq[0]])
 
         for i in range(k, len(nums)):
             if dq and dq[0] == i - k:
-                dq.popleft
+                dq.popleft()
             while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
-            
+
             dq.append(i)
             res.append(nums[dq[0]])
+
         return res
         
