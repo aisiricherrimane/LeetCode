17664a39a0c3942ab8fd50316dfae6aa63fdd6cf
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-06 23:34:15 -0500

    LC-search-in-rotated-sorted-array [Runtime: 0 ms; Memory: 18.1 MB]

--- array/search-in-rotated-sorted-array.py
+++ array/search-in-rotated-sorted-array.py
@@ -7,14 +7,15 @@ class Solution:
             mid = (r + l) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[l] <= nums[mid]:
-                if target < nums[mid] and target >= nums[l]:
+            
+            if nums[l] <= nums[mid]:
+                if target >= nums[l] and target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
             else:
-                if target > nums[mid] and target <= nums[r]:
+                if target <= nums[r] and target > nums[mid]:
                     l = mid + 1
                 else:
                     r = mid - 1
-        return -1        
+        return -1
