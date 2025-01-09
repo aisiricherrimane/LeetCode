f38a3e3a9d41c1555ebc8e7f16eab411b28c3b5d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-09 12:18:57 +0530

    LC-island-perimeter [Runtime: N/A; Memory: N/A]

--- breadth-first-search/island-perimeter.py
+++ breadth-first-search/island-perimeter.py
@@ -1,24 +1,17 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        row = len(grid)
-        col = len(grid[0])
-        self.cnt = 0
-        def dfs(r, c):
-            if r < 0 or r > row - 1 or c < 0 or c > col - 1:
-                return 1
-            elif grid[r][c] == 0:
-                return 1
-            elif grid[r][c] == -1:
-                return 0
-            else:
-                grid[r][c] = -1
-                return dfs(r + 1,c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
-
-        for r in range(row):
-            for c in range(col):
+        rows = len(grid)
+        cols = len(grid)
+        res = 0
+        
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 1:
-                    return dfs(r, c)
-
+                    res += 4
+                    if r > 0 and grid[r - 1][c] == 1:
+                        res -= 2
+                    if c >0 and grid[r][c - 1] == 1:
+                        res -= 2
+        return res
                     
-
         
