c962d179adb22603399cbaac85e1b4ed9b92dc44
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-09 14:33:57 +0530

    LC-surrounded-regions [Runtime: N/A; Memory: N/A]

--- breadth-first-search/surrounded-regions.py
+++ breadth-first-search/surrounded-regions.py
@@ -4,33 +4,31 @@ class Solution:
         Do not return anything, modify board in-place instead.
         
         """
-        if not board or not board[0]:
-            return 
         rows = len(board)
         cols = len(board[0])
 
-        def capture(r, c):
-            if 0 <= r < rows and 0 <= c < cols and board[r][c] == 'O':
-                board[r][c] = 'T'
-                capture(r + 1, c)
-                capture(r - 1, c)
-                capture(r, c + 1)
-                capture(r, c - 1)
+        def dfs(r, c):
+            board[r][c] = 'T'
+            for dr, dc in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
+                newr, newc = dr + r, dc + c
+                if newr >= 0 and newr < rows and newc >= 0 and newc < cols and board[newr][newc] == 'O':
+                    dfs(new, newc)
 
-        
+
         for r in range(rows):
             for c in [0, cols - 1]:
                 if board[r][c] == 'O':
-                    capture(r, c)
+                    dfs(r, c)
         for r in [0, rows - 1]:
             for c in range(cols):
                 if board[r][c] == 'O':
-                    capture(r, c)
+                    dfs(r, c)
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
-                elif board[r][c] == 'T':
+                if board[r][c] == 'T':
                     board[r][c] = 'O'
+                elif board[r][c] == 'O':
+                    board[r][c] = 'X'
+        return board
         
