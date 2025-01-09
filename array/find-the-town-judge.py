40bbf81461e6dea10731830a2781a3f6c0f6c951
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-09 13:08:54 +0530

    LC-find-the-town-judge [Runtime: 8 ms; Memory: 20.9 MB]

--- array/find-the-town-judge.py
+++ array/find-the-town-judge.py
@@ -1,10 +1,19 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        judge = trust[0][1]
+        N = n
+        if len(trust) < N - 1:
+            return -1
 
-        for common, maybe in trust:
-            if maybe != judge:
-                return -1
-        return judge
-            
+        indegree = [0] * (N + 1)
+        outdegree = [0] * (N + 1)
+
+        for a, b in trust:
+            indegree[b] += 1
+            outdegree[a] += 1
+        
+        for i in range(1, N + 1):
+            if indegree[i] == N - 1 and outdegree[i] == 0:
+                return i
+        return -1
+
         
