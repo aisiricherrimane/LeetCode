fee9cdd23b449f1ae24de423c285dc71a4ca2889
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-09 14:43:51 +0530

    LC-max-area-of-island [Runtime: 13 ms; Memory: 18.1 MB]

--- matrix/max-area-of-island.py
+++ matrix/max-area-of-island.py
@@ -5,13 +5,13 @@ class Solution:
         area = 0
 
         def dfs(r, c):
-            if r < 0 or r == rows or c < 0. or c == cols or grid[r][c] == 0:
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != 1:
                 return 0
             grid[r][c] = 0
             return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
-            
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     area = max(area, dfs(r, c))
-        return area        
+        return area
