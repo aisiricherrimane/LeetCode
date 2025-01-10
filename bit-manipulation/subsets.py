8d5867ef5d0306d34aa5cc360ee5ec34712eba95
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-10 15:18:52 +0530

    LC-subsets [Runtime: 0 ms; Memory: 18.1 MB]

--- bit-manipulation/subsets.py
+++ bit-manipulation/subsets.py
@@ -2,15 +2,15 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        def dfs(i, sub):
+        def dfs(i, subset):
             if i >= len(nums):
-                res.append(sub[:])
-                return 
-            sub.append(nums[i])
-            dfs(i + 1, sub)
-            sub.pop()
-            dfs(i + 1, sub)
-
+                res.append(subset[:])
+                return
+            subset.append(nums[i])
+            dfs(i + 1, subset)
+            subset.pop()
+            dfs(i + 1, subset)
         dfs(0, [])
+
         return res
-        
+            
