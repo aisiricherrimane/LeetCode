684f49b4e20592ada15517b98fc503a2dabeed5c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-10 12:46:11 +0530

    LC-accounts-merge [Runtime: 21 ms; Memory: 20.6 MB]

--- union-find/accounts-merge.py
+++ union-find/accounts-merge.py
@@ -1,49 +1,53 @@
 class UnionFind:
     def __init__(self, n):
         self.par = [i for i in range(n)]
-        self.rank = [1] * n
+        self.child = [0] * n
 
-    def find(self, x):
-        while x != self.par[x]:
-            self.par[x] = self.par[self.par[x]]
-            x = self.par[x]
-        return x
-    def union(self, e1, e2):
-        p1 = self.find(e1)
-        p2 = self.find(e2)
+    def find(self, n):
+        while n != self.par[n]:
+            self.par[n] = self.par[self.par[n]]
+            n = self.par[n]
+        return n
+    
+    def union(self, n1, n2):
+        p1 = self.find(n1)
+        p2 = self.find(n2)
+
         if p1 == p2:
             return False
-        if self.rank[p1] > self.rank[p2]:
-            self.par[p2] = p1
-            self.rank[p1] += self.rank[p2]
-        else:
-            self.par[p1] = p2
-            self.rank[p2] += self.rank[p1]
-        return True
         
+        if self.child[p1] > self.child[p2]:
+            self.child[p1] += 1
+            self.par[p2] = p1
+        else:
+            self.child[p2] += 1
+            self.par[p1] = p2
+        return True
+
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         uf = UnionFind(len(accounts))
-        email_to_ind = {}
+
+        emailtoacc = {}
 
         for i, a in enumerate(accounts):
             for e in a[1:]:
-                if e in email_to_ind:
-                    uf.union(i, email_to_ind[e])
+                if e in emailtoacc:
+                    uf.union(emailtoacc[e], i)
                 else:
-                    email_to_ind[e] = i
+                    emailtoacc[e] = i
         
-        emailGroup = collections.defaultdict(list)
-        for e, i in email_to_ind.items():
+        emailgroup = defaultdict(list)
+
+        for e, i in emailtoacc.items():
             leader = uf.find(i)
-            emailGroup[leader].append(e)
+            emailgroup[leader].append(e)
         
         res = []
-        for i, emails in emailGroup.items():
+        for i, emails in emailgroup.items():
             name = accounts[i][0]
-            res.append([name] + sorted(emailGroup[i])) # array concat
+            res.append([name] + sorted(emails))
         return res
-
 
 
         
