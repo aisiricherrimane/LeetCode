a9cbe573f0cf3905952c0d5ecb165bd91728b80e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-20 23:15:58 +0530

    LC-analyze-user-website-visit-pattern [Runtime: 14 ms; Memory: 18.4 MB]

--- hash-table/analyze-user-website-visit-pattern.py
+++ hash-table/analyze-user-website-visit-pattern.py
@@ -2,22 +2,19 @@ class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         data = sorted(zip(timestamp, username, website))
         user_web = defaultdict(list)
-        
+
         for _, user, web in data:
             user_web[user].append(web)
-
-        store = defaultdict(set)
-
-        for user, webs in user_web.items():
-            for pattern in set(combinations(webs, 3)):
-                store[pattern].add(user)
         
-        res = []
-        maxcount = 0
-        for pattern, users in store.items():
-            if len(users) > maxcount or (len(users) == maxcount and list(pattern) < res):
-                maxcount = len(users)
-                res = list(pattern)
-        return res
+        web_count = defaultdict(int)
+        for user, web in user_web.items():
+            for pattern in set(combinations(web, 3)):
+               web_count[pattern] += 1
+        
+        most_visited = sorted(web_count.keys(), key = lambda x:(-web_count[x], x))
 
+        return list(most_visited[0])
+         
+
+            
         
