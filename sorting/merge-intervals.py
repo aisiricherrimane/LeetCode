93aac23734e766346d5ab7ca3ac5eeaa23d4ca5f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-21 07:49:51 +0530

    LC-merge-intervals [Runtime: 7 ms; Memory: 20.9 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -1,5 +1,7 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x:x[0])
+
         res = [intervals[0]]
         intervals.pop(0)
 
