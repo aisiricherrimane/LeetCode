37e28a3169eb0f85804d590de9eca207ae0558b8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-21 06:14:57 +0530

    LC-identify-the-largest-outlier-in-an-array [Runtime: 111 ms; Memory: 28.1 MB]

--- counting/identify-the-largest-outlier-in-an-array.py
+++ counting/identify-the-largest-outlier-in-an-array.py
@@ -1,17 +1,17 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
-        total_sum = sum(nums)
-        nums_counts = Counter(nums)
-
+        sum_v = sum(nums)
+        num_count = Counter(nums)
         largest_outlier = float('-inf')
 
-        for num in nums_counts.keys():
-            potential = total_sum - 2 * num
+        for num in num_count.keys():
+            possible = sum_v - 2 * num
 
-            if potential in nums_counts:
-                if potential != num or nums_counts[num] > 1:
-                    largest_outlier = max(largest_outlier, potential)
+            if possible in num_count:
+                if possible != num or num_count[num] > 1:
+                    largest_outlier = max(largest_outlier, possible)
         return largest_outlier
+
 
 
         
