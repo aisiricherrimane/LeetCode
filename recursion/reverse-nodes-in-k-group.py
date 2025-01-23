cbc15e3d6aee8ddfcefedf74a5442a7a27d8e0d8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-24 00:21:38 +0530

    LC-reverse-nodes-in-k-group [Runtime: 0 ms; Memory: 18.4 MB]

--- recursion/reverse-nodes-in-k-group.py
+++ recursion/reverse-nodes-in-k-group.py
@@ -5,38 +5,29 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # Step 1: Check if there are at least k nodes to reverse
         count = 0
         curr = head
-        while curr and count < k:
+
+        while count < k and curr:
             curr = curr.next
             count += 1
         
-        # If we don't have k nodes, return head as it is
-        if count < k:
-            return head
-        
-        # Step 2: Reverse the first k nodes
-        prev = None
-        curr = head
-        for _ in range(k):
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        
-        # Step 3: Recursively reverse the next k nodes and connect them
-        head.next = self.reverseKGroup(curr, k)
-        
-        # prev is the new head of the reversed list
-        return prev
+        if count == k:
+            reversed_head = self.reverse(head, k)
+            head.next = self.reverseKGroup(curr, k)
+            return reversed_head
+        return head
     
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
-        while curr:
+    def reverse(self, node, k):
+        new_head = None
+        curr = node
+
+        while k:
             temp = curr.next
-            curr.next = prev
-            prev = curr
+            curr.next = new_head
+            new_head = curr
             curr = temp
-        return prev
+
+            k -= 1
+        return new_head
+        
