46b53f0fcab86c5454f32eb9f92b2e6b3fe10f14
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-23 05:38:32 +0530

    LC-insert-delete-getrandom-o1 [Runtime: 334 ms; Memory: 57.8 MB]

--- array/insert-delete-getrandom-o1.py
+++ array/insert-delete-getrandom-o1.py
@@ -2,37 +2,20 @@ from random import choice
 class RandomizedSet:
     def __init__(self):
         self.store = []
-        self.ind = {}
-        
-    def insert(self, val: int) -> bool:
-        if val in self.ind:
+
+    def insert(self, val):
+        if val not in self.store:
+            self.store.append(val)
+            return True
+        else:
             return False
-        self.store.append(val)
-        self.ind[val] = len(self.store) - 1
-        return True
 
-    def remove(self, val: int) -> bool:
-        if val not in self.ind:
+    def remove(self, val):
+        if val in self.store:
+            self.store.remove(val)
+            return True
+        else:
             return False
-        last_element = self.store[-1]
-        last_element_ind = len(self.store) - 1
-        val_ind = self.ind[val]
-        
-        self.store[val_ind] = last_element
-        self.ind[last_element] = val_ind
-
-        self.store.pop()
-        del self.ind[val]
-        return True
-
-    def getRandom(self) -> int:
+    
+    def getRandom(self):
         return random.choice(self.store)
-         
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
