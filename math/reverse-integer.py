2ee8d35279005d9c6a01b0aafc0109cfb596a7e6
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-23 06:07:41 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

---
+++ math/reverse-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = x * -1
+        
+        curr = 0
+
+        while x > 0:
+            last = x % 10
+            curr = curr * 10 + last
+            x = x // 10
+        
+        return curr * sign 
+        
