c225d84dbc34fafa16251e1775819499d173f33e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-23 18:36:12 +0530

    LC-unique-paths [Runtime: 0 ms; Memory: 17.7 MB]

--- combinatorics/unique-paths.py
+++ combinatorics/unique-paths.py
@@ -1,18 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        self.res = 0  
-        rows = m
-        cols = n
+        dp = [[1] * n for _ in range(m)]
 
-        def dfs(r, c):
-            if r >= 0 and r < rows and c >= 0 and c < cols:
-                if r == m - 1 and c == n - 1:
-                    self.res += 1
-                    return 
-                dfs(r + 1, c)
-                dfs(r, c + 1)
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
         
-        dfs(0, 0)
-        return self.res
+        return dp[m-1][n-1]
 
         
