169f3d877c2e31a35814d801224ee7a5b3b054be
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-24 18:47:53 +0530

    LC-find-the-town-judge [Runtime: N/A; Memory: N/A]

--- graph/find-the-town-judge.py
+++ graph/find-the-town-judge.py
@@ -1,19 +1,9 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        N = n
-        if len(trust) < N - 1:
-            return -1
+        visit = set()
+        temp = trust[0][1]
 
-        indegree = [0] * (N + 1)
-        outdegree = [0] * (N + 1)
-
-        for a, b in trust:
-            indegree[b] += 1
-            outdegree[a] += 1
-        
-        for i in range(1, N + 1):
-            if indegree[i] == N - 1 and outdegree[i] == 0:
-                return i
-        return -1
-
-        
+        for p, maybej in trust:
+            if temp != maybej:
+                return -1
+        return temp
