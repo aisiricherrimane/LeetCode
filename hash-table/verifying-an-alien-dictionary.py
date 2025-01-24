1e1ea9855e70efd519d57359df227261c6e35f4f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-24 18:45:09 +0530

    LC-verifying-an-alien-dictionary [Runtime: N/A; Memory: N/A]

--- hash-table/verifying-an-alien-dictionary.py
+++ hash-table/verifying-an-alien-dictionary.py
@@ -1,17 +1,21 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        letter_map = {char:i for i, char in enumerate(order)}
-
+        alien = {char:i for i, char in enumerate(order)}
+        
         for i in range(len(words) - 1):
             word1 = words[i]
             word2 = words[i + 1]
 
-            for k in range(min(len(word1), len(word2))):
-                if word1[k] != word2[k]:
-                    if letter_map[word1[k]] > letter_map[word2[k]]:
+            for j in range(min(len(word1), len(word2))):
+                if word1[j] != word2[j]:
+                    if alien[word1[j]] > alien[word2[j]]:
                         return False
-                    break
-            else:
-                if len(word1) > len(word2):
-                    return False
-        return True
+                    else:
+                        break
+            
+                else:
+                    if len(word1) > len(word2):
+                        return False
+        return True                
+
+        
