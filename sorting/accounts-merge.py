0b2c91b844666bb252a492e585fc8ea7d3b2ca73
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-25 00:22:40 +0530

    LC-accounts-merge [Runtime: 19 ms; Memory: 20.5 MB]

--- sorting/accounts-merge.py
+++ sorting/accounts-merge.py
@@ -1,17 +1,17 @@
 class UnionFind:
     def __init__(self, n):
         self.par = [i for i in range(n)]
-        self.child = [0] * n
+        self.child = [1] * n
 
-    def find(self, n):
+    def find(self,n):
+        n = self.par[n]
         while n != self.par[n]:
             self.par[n] = self.par[self.par[n]]
             n = self.par[n]
         return n
     
-    def union(self, n1, n2):
-        p1 = self.find(n1)
-        p2 = self.find(n2)
+    def union(self,n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
 
         if p1 == p2:
             return False
@@ -23,6 +23,7 @@ class UnionFind:
             self.child[p2] += 1
             self.par[p1] = p2
         return True
+        
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
@@ -38,14 +39,15 @@ class Solution:
                     emailtoacc[e] = i
         
         emailgroup = defaultdict(list)
+        print(emailgroup)
 
         for e, i in emailtoacc.items():
             leader = uf.find(i)
             emailgroup[leader].append(e)
         
         res = []
-        for i, emails in emailgroup.items():
-            name = accounts[i][0]
+        for leader_i, emails in emailgroup.items():
+            name = accounts[leader_i][0]
             res.append([name] + sorted(emails))
         return res
 
