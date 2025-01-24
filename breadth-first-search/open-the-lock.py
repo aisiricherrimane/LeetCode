c3700be72ffcca8d709207654982ec3d7a80b235
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-24 20:59:22 +0530

    LC-open-the-lock [Runtime: 417 ms; Memory: 19.1 MB]

--- breadth-first-search/open-the-lock.py
+++ breadth-first-search/open-the-lock.py
@@ -3,28 +3,29 @@ class Solution:
         if '0000' in deadends:
             return -1
 
-        q = deque()
-        q.append(['0000', 0])
-        visit = set(deadends)
-
-        def children(n):
+        def children(number):
             res = []
             for i in range(4):
-                digit = str((int(n[i]) + 1) % 10)
-                res.append(n[:i] + digit + n[i + 1:])
-                digit = str((int(n[i]) - 1 + 10) % 10)
-                res.append(n[:i] + digit + n[i + 1:])
+                digit = str((int(number[i]) + 1) % 10)
+                res.append(number[:i] + digit + number[i + 1:])
+                digit = str((int(number[i]) - 1 + 10) % 10)
+                res.append(number[:i] + digit + number[i + 1:])
             return res
 
 
+        q = deque([('0000', 0)])
+        visit = set(deadends)
 
         while q:
-            lock, turns = q.popleft()
+            number, turns = q.popleft()
 
-            if lock == target:
+            if number == target:
                 return turns
-            for number in children(lock):
-                if number not in visit:
-                    visit.add(number)
-                    q.append([number, turns + 1])
+            
+            for n in children(number):
+                if n not in visit:
+                    q.append([n, turns + 1])
+                    visit.add(n)
         return -1
+            
+        
