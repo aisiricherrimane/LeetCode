80d7ac17e678edc393ddad864687f389f08c67c1
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-25 05:22:14 +0530

    LC-word-ladder [Runtime: N/A; Memory: N/A]

--- hash-table/word-ladder.py
+++ hash-table/word-ladder.py
@@ -6,28 +6,33 @@ class Solution:
             wordList.append(beginWord)
 
         adj = defaultdict(list)
+            
+
         for word in wordList:
-            for i in range(len(word)):
+            for i in len(word):
                 pattern = word[:i] + '*' + word[i + 1:]
                 adj[pattern].append(word)
         
-        q = deque([beginWord])
+        q = deque()
+        q.append(beginWord)
         visit = set()
         length = 1
-
         while q:
             for _ in range(len(q)):
                 word = q.popleft()
+
                 if word == endWord:
-                    return length
+                    return length 
+                
                 if word in visit:
                     continue
                 visit.add(word)
 
-                for i in range(len(word)):
+                for i in len(word):
                     pattern = word[:i] + '*' + word[i + 1:]
                     for neiW in adj[pattern]:
-                        q.append(neiW)
+                        q.append(word)
             length += 1
-            
         return 0
+
+
