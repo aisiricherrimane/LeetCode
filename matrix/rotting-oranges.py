9625dfdaeea9ff2405ad937b7d5bca38367afc94
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-26 01:49:30 +0530

    LC-rotting-oranges [Runtime: 3 ms; Memory: 17.6 MB]

--- matrix/rotting-oranges.py
+++ matrix/rotting-oranges.py
@@ -3,23 +3,27 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         fresh = 0
+        time = 0
         q = deque()
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     fresh += 1
-                elif grid[r][c] == 2:
+                if grid[r][c] == 2:
                     q.append((r, c))
-        time = 0
+        
         while q and fresh > 0:
             for _ in range(len(q)):
                 r, c = q.popleft()
-                for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    newR, newC = dr + r, dc + c
-                    if newR >= 0 and newR < rows and newC >= 0 and newC < cols and grid[newR][newC] == 1:
-                        grid[newR][newC] = 2
-                        q.append((newR, newC))
+
+                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
                         fresh -= 1
+                        q.append((nr, nc))
+            
             time += 1
         return time if fresh == 0 else -1
+        
