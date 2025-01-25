5053e509289030f8514df2eaed59c28f80b236ec
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-25 06:18:14 +0530

    LC-surrounded-regions [Runtime: 3 ms; Memory: 21.4 MB]

--- array/surrounded-regions.py
+++ array/surrounded-regions.py
@@ -2,33 +2,33 @@ class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
-        
         """
         rows = len(board)
         cols = len(board[0])
+        q = deque()
 
         def dfs(r, c):
-            board[r][c] = 'T'
-            for dr, dc in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
-                newr, newc = dr + r, dc + c
-                if newr >= 0 and newr < rows and newc >= 0 and newc < cols and board[newr][newc] == 'O':
-                    dfs(newr, newc)
-
+            if 0 <= r < rows and 0 <= c < cols and board[r][c] == 'O':
+                board[r][c] = 'T'
+                dfs(r + 1, c)
+                dfs(r - 1, c)
+                dfs(r,c + 1)
+                dfs(r, c - 1)
 
         for r in range(rows):
             for c in [0, cols - 1]:
                 if board[r][c] == 'O':
                     dfs(r, c)
+        
         for r in [0, rows - 1]:
             for c in range(cols):
                 if board[r][c] == 'O':
                     dfs(r, c)
-        
+
         for r in range(rows):
             for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
-                elif board[r][c] == 'O':
-                    board[r][c] = 'X'
-        return board
-        
