fbc3d4f59c3330a35c6c89a26765f07e4788dd87
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-25 05:42:14 +0530

    LC-number-of-islands [Runtime: 232 ms; Memory: 20.3 MB]

--- depth-first-search/number-of-islands.py
+++ depth-first-search/number-of-islands.py
@@ -1,22 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        islands = 0 
+        row = len(grid)
+        col = len(grid[0])
+        islands = 0
 
         def dfs(r, c):
-            if r >= 0 and r < rows and c >= 0 and c < cols and grid[r][c] == '1':
-                grid[r][c] = '0'
+            if r >= 0 and r < row and c >= 0 and c < col and grid[r][c] == '1':
+                grid[r][c] = 0
                 dfs(r + 1, c)
                 dfs(r - 1, c)
                 dfs(r, c + 1)
                 dfs(r, c - 1)
-            
 
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(row):
+            for c in range(col):
                 if grid[r][c] == '1':
                     dfs(r, c)
                     islands += 1
         return islands
-        
