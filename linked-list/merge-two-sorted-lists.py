7fffb0f05963a0ac3374682818802f42ab819539
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-28 07:44:47 -0500

    LC-merge-two-sorted-lists [Runtime: N/A; Memory: N/A]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -7,20 +7,24 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
         temp = dummy
-        l1 = list1
-        l2 = list2
-        while l1 and l2:
-            if l1.val < l2.val:
-                temp.next = ListNode(l1.val)
-                l1 = l1.next
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = ListNode(list1.val)
+                list1 = list1.next
             else:
-                temp.next = ListNode(l2.val)
-                l2 = l2.next
+                temp.next = ListNode(list2.val)
+                list2 = list2.next
+            
             temp = temp.next
         
-        if l1:
-            temp.next = l1
-        if l2:
-            temp.next = l2
-
+        if list1 and not list2:
+            temp.next = ListNode(list1.val)
+            list1 = list1.next
+            temp = temp.next
+        elif list2 and not list1:
+            temp.next = ListNode(list2.val)
+            list2 = list2.next
+            temp = temp.next
+        
         return dummy.next
