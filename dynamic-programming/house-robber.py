42ab250b4bdb4998d25457afd307bed08ee4de51
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-28 13:54:18 -0500

    LC-house-robber [Runtime: 0 ms; Memory: 17.8 MB]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -4,8 +4,7 @@ class Solution:
         rob2 = 0
 
         for n in nums:
-            temp = max(n + rob1, rob2)
+            temp = max(rob1 + n, rob2)
             rob1 = rob2
             rob2 = temp
         return rob2
-        
