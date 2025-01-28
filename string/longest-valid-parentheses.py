1fe9403617639326e20c65b432a48cf7fc6b20a1
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-28 18:15:38 -0500

    LC-longest-valid-parentheses [Runtime: 11 ms; Memory: 17.7 MB]

--- string/longest-valid-parentheses.py
+++ string/longest-valid-parentheses.py
@@ -1,22 +1,33 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        for char in s:
-            if char == "(":
-                stack.append("(")
-            elif stack and stack[-1] == "(":
-                stack.pop()
-            else:
-                return False
-        return len(stack) == 0
-
     def longestValidParentheses(self, s: str) -> int:
-        maxlen = 0
+        left = 0
+        right = 0
+        maxlength = 0
+
         for i in range(len(s)):
-            for j in range(i + 2, len(s) + 1, 2):
-                if self.isValid(s[i:j]):
-                    maxlen = max(maxlen, j - i)
-        return maxlen
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                maxlength = max(maxlength, 2 * right)
+            elif right > left:
+                left = right = 0
+        
+        left = 0
+        right = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                maxlength = max(maxlength, 2 * left)
+            elif left > right:
+                left = right = 0
+        return maxlength
+            
 
     
                 
