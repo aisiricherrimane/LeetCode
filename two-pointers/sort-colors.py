cb728b410b69917e070802d634d3d3ad731fafdb
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-28 07:29:59 -0500

    LC-sort-colors [Runtime: N/A; Memory: N/A]

--- two-pointers/sort-colors.py
+++ two-pointers/sort-colors.py
@@ -7,17 +7,12 @@ class Solution:
         l = 0
         r = len(nums) - 1
 
-        def swap(a, b):
-            temp = nums[b]
-            nums[b] = nums[a]
-            nums[a] = temp
-
         while i <= r:
             if nums[i] == 0:
-                swap(i, l)
+                nums[l], nums[i] = nums[i], nums[l]
                 l += 1
             if nums[i] == 2:
-                swap(i, r)
+                nums[r], nums[i] = nums[i], nums[r]
                 r -= 1
             i += 1
         return nums
