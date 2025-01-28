a2464422af96cab1aa544058fac2459aebcc6d0c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-28 14:12:56 -0500

    LC-sliding-window-maximum [Runtime: 135 ms; Memory: 31.2 MB]

--- sliding-window/sliding-window-maximum.py
+++ sliding-window/sliding-window-maximum.py
@@ -3,12 +3,15 @@ class Solution:
         dq = deque()
         res = []
         for i in range(k):
-            while dq and nums[dq[-1]] < nums[i]:
+            while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
         res.append(nums[dq[0]])
 
         for i in range(k, len(nums)):
+            if dq and dq[0] == i - k:
+                dq.popleft()
+    
             while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
             dq.append(i)
