016faf8dc9cbe63d7565646e90c8919096c09200
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-28 14:11:55 -0500

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/sliding-window-maximum.py
+++ heap-priority-queue/sliding-window-maximum.py
@@ -2,21 +2,15 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         dq = deque()
         res = []
-
         for i in range(k):
-            while dq and nums[i] >= nums[dq[-1]]:
+            while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
             dq.append(i)
         res.append(nums[dq[0]])
-        
-        for i in range(k, len(nums)):
-            if dq and dq[0] == i - k:
-                dq.popleft()
 
-            while dq and nums[i] >= nums[dq[-1]]:
+        for i in range(k, len(nums)):
+            while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
             dq.append(i)
             res.append(nums[dq[0]])
         return res
-            
-
