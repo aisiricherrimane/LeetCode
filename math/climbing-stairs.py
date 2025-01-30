f7a7486eabcce598fa5ab06c67b00320cd0a5cb3
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-30 07:51:35 -0500

    LC-climbing-stairs [Runtime: 0 ms; Memory: 17.5 MB]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -1,8 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        n1, n2 = 1, 0
+        n2 = 0
+        n1 = 1
+
         for _ in range(n):
             temp = n1
             n1 = n1 + n2
             n2 = temp
-        return n1      
+        return n1
