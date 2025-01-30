ecd82e30396e86a0515bc8fd49165b122af6ea9f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-30 16:21:09 -0500

    LC-two-sum [Runtime: 0 ms; Memory: 18.8 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         temp = {}
+
         for i, n in enumerate(nums):
             diff = target - n
             if diff in temp:
-                return temp[diff], i
-            else:
-                temp[n] = i
+                return [i, temp[diff]]
+            temp[n] = i
         return -1
