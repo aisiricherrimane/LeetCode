98f683c3992f25d30252b759413c4b4514c1f4ab
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-30 08:10:32 -0500

    LC-word-ladder [Runtime: 63 ms; Memory: 20.9 MB]

--- hash-table/word-ladder.py
+++ hash-table/word-ladder.py
@@ -1,13 +1,11 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
-            return 0
         if beginWord not in wordList:
             wordList.append(beginWord)
+        if endWord not in wordList:
+            return 0
 
         adj = defaultdict(list)
-            
-
         for word in wordList:
             for i in range(len(word)):
                 pattern = word[:i] + '*' + word[i + 1:]
@@ -16,25 +14,23 @@ class Solution:
         q = deque()
         q.append(beginWord)
         visit = set()
-        length = 1
+        l = 1
 
         while q:
             for _ in range(len(q)):
                 word = q.popleft()
 
                 if word == endWord:
-                    return length 
-                
+                    return l
                 if word in visit:
                     continue
-
                 visit.add(word)
 
                 for i in range(len(word)):
                     pattern = word[:i] + '*' + word[i + 1:]
-                    for neiW in adj[pattern]:
-                        q.append(neiW)
-            length += 1
+                    for w in adj[pattern]:
+                        q.append(w)
+            l += 1
         return 0
 
-
+       
