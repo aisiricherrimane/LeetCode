44b97088b237048260bb7411bb4e4308c915ef49
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-30 17:35:59 -0500

    LC-reorganize-string [Runtime: 3 ms; Memory: 18 MB]

--- hash-table/reorganize-string.py
+++ hash-table/reorganize-string.py
@@ -3,16 +3,15 @@ class Solution:
         char_count = Counter(s)
         max_count, letter = 0, ''
 
-        for char, count in char_count.items():
-            if count > max_count:
-                max_count = count
+        for char, c_count in char_count.items():
+            if c_count > max_count:
+                max_count = c_count
                 letter = char
-        
         if max_count > (len(s) + 1) // 2:
             return ''
-        
-        ans = [''] * len(s)
-        index = 0
+
+        ans = [''] *len(s)
+        index = 0 
 
         while char_count[letter] != 0:
             ans[index] = letter
@@ -21,15 +20,12 @@ class Solution:
         
         for char, count in char_count.items():
             while count > 0:
-                if index >= len(s):
+                if index >= len(ans):
                     index = 1
+
                 ans[index] = char
                 index += 2
                 count -= 1
         return ''.join(ans)
 
-
-
-        
-        
         
