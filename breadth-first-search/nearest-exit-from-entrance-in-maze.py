2c7155c683c5f5e64e4c483b0b7b0f0febb48e2f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-31 11:16:45 -0500

    LC-nearest-exit-from-entrance-in-maze [Runtime: 93 ms; Memory: 19.6 MB]

--- breadth-first-search/nearest-exit-from-entrance-in-maze.py
+++ breadth-first-search/nearest-exit-from-entrance-in-maze.py
@@ -1,27 +1,25 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-
-        q = deque([(entrance[0], entrance[1], 0)])
+        q = deque()
+        q.append(entrance)
         visit = set()
         visit.add((entrance[0], entrance[1]))
-        rows = len(maze)
-        cols = len(maze[0])
+        row = len(maze)
+        col = len(maze[0])
+        step = 0
 
         while q:
-            r, c, steps = q.popleft()
-            
-            if (r, c) != (entrance[0], entrance[1]) and (r in [0, rows - 1] or c in [0, cols - 1]):
-                return steps
+            for _ in range(len(q)):
+                r, c = q.popleft()
 
-            for dr, dc in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
-                newR, newC = r + dr, c + dc
-                if 0 <= newR < rows and 0 <= newC < cols and maze[newR][newC] == '.' and (newR, newC) not in visit:
-                    visit.add((newR, newC))
-                    q.append((newR, newC, steps + 1))
+                if (r, c) != (entrance[0], entrance[1]) and (r in [0, row - 1] or c in [0, col - 1]):
+                    return step
 
-        
-        return -1  # If no exit is reachable
-            
-
-
-        
+                for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < row and 0 <= nc < col and (nr, nc) not in visit and maze[nr][nc] == '.':
+                        q.append((nr, nc))
+                        visit.add((nr, nc))
+                
+            step += 1
+        return -1
