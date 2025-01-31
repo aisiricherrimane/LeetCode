7f5491c63770f18090a4d4beec65a23afda02f36
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-01-31 08:48:30 -0500

    LC-number-of-islands [Runtime: 236 ms; Memory: 20.3 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -1,12 +1,12 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
         row = len(grid)
         col = len(grid[0])
-        islands = 0
 
         def dfs(r, c):
             if r >= 0 and r < row and c >= 0 and c < col and grid[r][c] == '1':
-                grid[r][c] = 0
+                grid[r][c] = '0'
                 dfs(r + 1, c)
                 dfs(r - 1, c)
                 dfs(r, c + 1)
@@ -18,3 +18,4 @@ class Solution:
                     dfs(r, c)
                     islands += 1
         return islands
+        
