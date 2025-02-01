744893c3804bea25e6ffbf7ae93f217260fd33c7
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-01 14:03:56 -0500

    LC-merge-k-sorted-lists [Runtime: 43 ms; Memory: 21.1 MB]

--- heap-priority-queue/merge-k-sorted-lists.py
+++ heap-priority-queue/merge-k-sorted-lists.py
@@ -5,37 +5,42 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
+        if not lists:
             return None
-
         while len(lists) > 1:
-            mergedlist = []
+            merged_list = []
             for i in range(0, len(lists), 2):
-                list1 = lists[i]
-                list2 = lists[i + 1] if i + 1 < len(lists) else None
-                mergedlist.append(self.mergetwo(list1, list2))
-            lists = mergedlist
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else 0
+                merged_list.append(self.mtwo(l1, l2))
+            lists = merged_list
         return lists[0]
     
-    def mergetwo(self, l1, l2):
+    def mtwo(self, l1, l2):
         dummy = ListNode()
-        curr = dummy
+        temp = dummy
 
         while l1 and l2:
-            if l1.val > l2.val:
-                curr.next = ListNode(l2.val)
-                l2 = l2.next
-            else:
-                curr.next = ListNode(l1.val)
+            if l1.val < l2.val:
+                temp.next = ListNode(l1.val)
                 l1 = l1.next
-            curr = curr.next
+            else:
+                temp.next = ListNode(l2.val)
+                l2 = l2.next
+            temp = temp.next
         
         if l1:
-            curr.next = l1
-        else:
-            curr.next = l2
+            temp.next = l1 
+            l1 = l1.next
+            temp = temp.next
+        if l2:
+            temp.next = l2
+            l2 = l2.next
+            temp = temp.next
+        
         return dummy.next
 
+        
 
         
 
