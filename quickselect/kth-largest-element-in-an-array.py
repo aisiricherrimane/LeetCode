ba2e5de4465c1a1351fec927da94bd769a39bc31
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-01 15:35:14 -0500

    LC-kth-largest-element-in-an-array [Runtime: N/A; Memory: N/A]

--- quickselect/kth-largest-element-in-an-array.py
+++ quickselect/kth-largest-element-in-an-array.py
@@ -1,23 +1,22 @@
+from typing import List
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-    
+        k = len(nums) - k  # Convert k-th largest to index
+
         def quickSelect(l, r):
-            pivot, p = nums[r], l
+            pivot, p = nums[r], l  # Pick last element as pivot
             for i in range(l, r):
                 if nums[i] <= pivot:
-                    nums[i], nums[p] = nums[p], nums[i]
+                    nums[i], nums[p] = nums[p], nums[i]  # Swap elements
                     p += 1
-            nums[p], nums[r] = nums[r], nums[p]
+            nums[p], nums[r] = nums[r], nums[p]  # Place pivot in correct position
 
-            if p > k:
-                return quickSelect(l, p - 1)
+            if p > k: 
+                return quickSelect(l, p - 1)  # Search left part
             elif p < k:
-                return quickSelect(p + 1, r)
+                return quickSelect(p + 1, r)  # Search right part
             else:
-                return nums[p]
-        
+                return nums[p]  # Found the k-th largest element
+
         return quickSelect(0, len(nums) - 1)
-
-     
-
