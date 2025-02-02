225d7c34f0cafda1deed41a0eadc312f62fd45a4
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-02 13:11:35 -0500

    LC-top-k-frequent-elements [Runtime: 8 ms; Memory: 24.4 MB]

--- hash-table/top-k-frequent-elements.py
+++ hash-table/top-k-frequent-elements.py
@@ -1,16 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
+
         freq = [[] for i in range(len(nums) + 1)]
-        for num, c in count.items():
-            freq[c].append(num)
-        
+
+        for n, f in count.items():
+            freq[f].append(n)
+
         res = []
         for i in range(len(freq) - 1, -1, -1):
-            for char in freq[i]:
-                res.append(char)
-                k -= 1
-                if k == 0:
-                    return res
-
-      
+            for n in freq[i]:
+                    res.append(n)
+                    k -= 1
+                    if k == 0:
+                        return res
+            
