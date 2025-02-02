2f4804aca6d8e2074fac48c72cb9575694ef3300
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-02 16:18:22 -0500

    LC-reorganize-string [Runtime: 0 ms; Memory: 17.9 MB]

--- greedy/reorganize-string.py
+++ greedy/reorganize-string.py
@@ -1,31 +1,31 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         char_count = Counter(s)
-        max_count, letter = 0, ''
+        max_count = 0
+        letter = ''
 
-        for char, c_count in char_count.items():
-            if c_count > max_count:
-                max_count = c_count
-                letter = char
+        for c, f in char_count.items():
+            if f > max_count:
+                max_count = f
+                letter = c
+        ind = 0
+        res = [''] * len(s)
+
         if max_count > (len(s) + 1) // 2:
             return ''
-
-        ans = [''] *len(s)
-        index = 0 
-
-        while char_count[letter] != 0:
-            ans[index] = letter
-            index += 2
-            char_count[letter] -= 1
         
+        while char_count[letter] != 0:
+            res[ind] = letter
+            ind += 2
+            char_count[letter] -= 1
+
         for char, count in char_count.items():
             while count > 0:
-                if index >= len(ans):
-                    index = 1
-
-                ans[index] = char
-                index += 2
+                if ind >= len(res):
+                    ind = 1
+                res[ind] = char
                 count -= 1
-        return ''.join(ans)
+                ind += 2
+        return ''.join(res)
 
         
