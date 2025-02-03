cce7b62546d9d95a823bb0b5758afa9d5357d837
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-03 08:27:49 -0500

    LC-climbing-stairs [Runtime: 0 ms; Memory: 17.8 MB]

--- memoization/climbing-stairs.py
+++ memoization/climbing-stairs.py
@@ -1,10 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        n2 = 0
-        n1 = 1
+        n1, n2 = 1, 0
 
-        for _ in range(n):
+        for i in range(n):
             temp = n1
             n1 = n1 + n2
             n2 = temp
         return n1
+        
