932bd561b0d69f0185fe6c944e63eaeed4eeac0b
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-03 10:35:40 -0500

    LC-sliding-window-maximum [Runtime: 152 ms; Memory: 31.2 MB]

--- monotonic-queue/sliding-window-maximum.py
+++ monotonic-queue/sliding-window-maximum.py
@@ -1,19 +1,20 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque()
+        q = deque()
         res = []
+
         for i in range(k):
-            while dq and nums[i] >= nums[dq[-1]]:
-                dq.pop()
-            dq.append(i)
-        res.append(nums[dq[0]])
+            while q and nums[q[-1]] <= nums[i]:
+                q.pop()
+            q.append(i)
+        res.append(nums[q[0]])
 
         for i in range(k, len(nums)):
-            if dq and dq[0] == i - k:
-                dq.popleft()
-    
-            while dq and nums[dq[-1]] < nums[i]:
-                dq.pop()
-            dq.append(i)
-            res.append(nums[dq[0]])
+            while q and q[0] == i - k:
+                q.popleft()
+            while q and nums[q[-1]] <= nums[i]:
+                q.pop()
+            q.append(i)
+            res.append(nums[q[0]])
         return res
+            
