b08eb6b92db77c56b0a26e2c86da60b7ed60272b
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-03 08:00:06 -0500

    LC-jump-game-ii [Runtime: 7 ms; Memory: 18.5 MB]

--- array/jump-game-ii.py
+++ array/jump-game-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        cur_far = 0
+        curr_end = 0
+        curr_far = 0
         count = 0
-        cur_end = 0
 
         for i in range(len(nums) - 1):
-            cur_far = max(cur_far, i + nums[i])
+            curr_far = max(curr_far, i + nums[i])
 
-            if i == cur_end:
+            if i == curr_end:
                 count += 1
-                cur_end = cur_far
-
+                curr_end = curr_far
         return count
