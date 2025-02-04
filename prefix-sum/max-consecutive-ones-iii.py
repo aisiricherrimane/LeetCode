07998da7ffd22d341fa6707235eda1d2acb0621f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-04 16:28:35 -0500

    LC-max-consecutive-ones-iii [Runtime: 76 ms; Memory: 18.4 MB]

--- prefix-sum/max-consecutive-ones-iii.py
+++ prefix-sum/max-consecutive-ones-iii.py
@@ -6,7 +6,7 @@ class Solution:
         for r, num in enumerate(nums):
             k -= 1 - num
 
-            if k < 0:
+            while k < 0:
                 k += 1 - nums[l]
                 l += 1
             
