c1fd4044e952842b532159f9a8eed0a84ba30de4
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-05 07:14:44 -0500

    LC-sqrtx [Runtime: 0 ms; Memory: 17.7 MB]

--- binary-search/sqrtx.py
+++ binary-search/sqrtx.py
@@ -2,18 +2,18 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
-        l = 1
-        r = x // 2
-        while l <= r:
-            m = (l + r) // 2
-            if m * m == x:
-                return m
-            elif m * m < x:
-                l = m + 1
+        
+        left = 2
+        right = x // 2
+
+        while left <= right:
+            pivot = (left + right) // 2
+            num = pivot * pivot
+
+            if num > x:
+                right = pivot - 1
+            elif num < x:
+                left = pivot + 1
             else:
-                r = m - 1
-        return r
-
-
-        
-        
+                return pivot
+        return right
