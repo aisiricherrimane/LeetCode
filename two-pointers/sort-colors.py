8915c96fec7bd9f8aa8be8c1d4a383f3998b0e62
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-05 07:33:38 -0500

    LC-sort-colors [Runtime: 4 ms; Memory: 17.7 MB]

--- two-pointers/sort-colors.py
+++ two-pointers/sort-colors.py
@@ -3,19 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        l = 0
-        r = len(nums) - 1
+        n = len(nums)
 
-        while i <= r:
-            if nums[i] == 0:
-                nums[l], nums[i] = nums[i], nums[l]
-                l += 1
-            elif nums[i] == 2:
-                nums[r], nums[i] = nums[i], nums[r]
-                r -= 1
-                i -= 1
-            i += 1
+        for i in range(n):
+            swapped = False
+            for j in range(0, n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+            if not swapped: break
         return nums
-
-        
+    
