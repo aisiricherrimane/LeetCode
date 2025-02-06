f00548556be4af87335a961b21f7224e7f27d256
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-06 18:39:48 -0500

    LC-combination-sum [Runtime: 11 ms; Memory: 18 MB]

--- array/combination-sum.py
+++ array/combination-sum.py
@@ -1,18 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        def backtrack(i, total, subset):
+        result = []
+        def backtrack(i, curr, total):
             if total == target:
-                res.append(subset[:])
+                result.append(curr[:])
                 return 
             if total > target:
                 return
             if i < len(candidates):
-                subset.append(candidates[i])
-                backtrack(i, total + candidates[i], subset)
-                subset.pop()
-                backtrack(i + 1, total , subset)
+                curr.append(candidates[i])
+                backtrack(i, curr, total + candidates[i])
+                curr.pop()
+                backtrack(i + 1, curr, total)
+        backtrack(0, [], 0)
+        return result
 
-        backtrack(0, 0, [])
-
-        return res
+        
