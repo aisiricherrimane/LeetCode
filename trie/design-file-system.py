8d7981782e3457e598fb30e2dfdf92904a4c46a0
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-07 19:00:24 -0500

    LC-design-file-system [Runtime: 12 ms; Memory: 25.3 MB]

--- trie/design-file-system.py
+++ trie/design-file-system.py
@@ -1,21 +1,20 @@
 class FileSystem:
-
     def __init__(self):
-        self.paths = {}
+        self.path = {}
 
     def createPath(self, path: str, value: int) -> bool:
-        if path == '/' or len(path) == 0 or path in self.paths:
+        if path == '/' or len(path) == 0 or path in self.path:
             return False
         parent = path[:path.rfind('/')]
-        if len(parent) > 1 and parent not in self.paths:
+        if len(parent) > 1 and parent not in self.path:
             return False
-        
-        self.paths[path] = value
+        self.path[path] = value
         return True
-
+    
     def get(self, path: str) -> int:
-        return self.paths.get(path, -1)
+        return self.path.get(path, -1)
         
+    
 
 
 # Your FileSystem object will be instantiated and called as such:
