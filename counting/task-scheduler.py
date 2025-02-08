8d137f376cd0ac130f5142b9d41acd1a45530cc5
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-08 12:46:53 -0500

    LC-task-scheduler [Runtime: 150 ms; Memory: 19.2 MB]

--- counting/task-scheduler.py
+++ counting/task-scheduler.py
@@ -1,20 +1,22 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         task_count = Counter(tasks)
-        heap = [-c for t, c in task_count.items()]
-
+        heap = [-c for t,c in task_count.items()]
         heapq.heapify(heap)
-        q = deque()
         time = 0
+        queue = deque()
 
-        while heap or q:
+        while heap or queue:
             time += 1
+
             if heap:
-                num = heapq.heappop(heap) + 1
-                if num != 0:
-                    q.append([time + n, num])
-            
-            if q and q[0][0] == time:
-                heapq.heappush(heap, q.popleft()[1])
+                num = heapq.heappop(heap)
+                if (num + 1) != 0:
+                    queue.append([time + n, num + 1])
+
+            if queue and queue[0][0] == time:
+                heapq.heappush(heap, queue.popleft()[1])
         return time
-            
+
+
+        
