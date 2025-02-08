0c91e3bdd98c86683e4d9b8a941a5fe31ce86a1a
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-07 19:18:03 -0500

    LC-unique-paths-ii [Runtime: 2 ms; Memory: 17.8 MB]

--- dynamic-programming/unique-paths-ii.py
+++ dynamic-programming/unique-paths-ii.py
@@ -3,19 +3,20 @@ class Solution:
         row = len(obstacleGrid)
         col = len(obstacleGrid[0])
 
-        if obstacleGrid[row - 1][col - 1] == 1:
+        if obstacleGrid[0][0] == 1 or obstacleGrid[row - 1][col - 1] == 1:
             return 0
-            
-        dp = [[1] * col for _ in range(row)]
+
+        dp = [[0] * col for _ in range(row)]
+        dp[0][0] = 1 
         
-        for r in range(1, row):
-            for c in range(1, col):
+        for r in range(row):
+            for c in range(col):
                 if obstacleGrid[r][c] == 1:
-                    continue
-                if obstacleGrid[r - 1][c] == 1:
-                    dp[r - 1][c] = 0
-                if obstacleGrid[r][c - 1] == 1:
-                    dp[r][c - 1] = 0
-                
-                dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+                    dp[r][c] = 0  # No path if there's an obstacle
+                else:
+                    if r > 0:
+                        dp[r][c] += dp[r - 1][c]
+                    if c > 0:
+                        dp[r][c] += dp[r][c - 1]
+
         return dp[row - 1][col - 1]
