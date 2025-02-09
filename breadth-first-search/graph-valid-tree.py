8bc306ee2828d22dea04697c0761912a6976e2ab
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-09 17:37:43 -0500

    LC-graph-valid-tree [Runtime: 4 ms; Memory: 20 MB]

--- breadth-first-search/graph-valid-tree.py
+++ breadth-first-search/graph-valid-tree.py
@@ -4,15 +4,20 @@ class Solution:
 
         for s, e in edges:
             adj[s].append(e)
+            adj[e].append(s)
         
         visit = set()
         def dfs(node, prev):
             if node in visit:
                 return False
+
             visit.add(node)
+
             for neiN in adj[node]:
+                if neiN == prev:
+                    continue
                 if not dfs(neiN, node):
                     return False
             return True
         
-        return dfs(0, -1)
+        return dfs(0, -1) and len(visit) == n
