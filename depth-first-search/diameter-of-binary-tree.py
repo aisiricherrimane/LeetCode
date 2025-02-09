5d2aac1b475cce911091ad7752a8778731604605
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-09 14:48:56 -0500

    LC-diameter-of-binary-tree [Runtime: 3 ms; Memory: 20.8 MB]

--- depth-first-search/diameter-of-binary-tree.py
+++ depth-first-search/diameter-of-binary-tree.py
@@ -11,12 +11,10 @@ class Solution:
         def dfs(node):
             if not node:
                 return 0
-            
             left = dfs(node.left)
             right = dfs(node.right)
 
             self.res = max(self.res, left + right)
-
             return 1 + max(left, right)
         dfs(root)
         return self.res
