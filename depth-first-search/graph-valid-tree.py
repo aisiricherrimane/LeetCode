f033e20bcb63a008eac997096d7001f22e0d4ea8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-09 17:36:28 -0500

    LC-graph-valid-tree [Runtime: N/A; Memory: N/A]

--- depth-first-search/graph-valid-tree.py
+++ depth-first-search/graph-valid-tree.py
@@ -1,24 +1,18 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        adj = {i : [] for i in range(n)}
+        adj = {i:[] for i in range(n)}
 
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
-
+        for s, e in edges:
+            adj[s].append(e)
+        
         visit = set()
-
         def dfs(node, prev):
             if node in visit:
                 return False
-            
             visit.add(node)
-            for neiNodes in adj[node]:
-                if neiNodes == prev:
-                    continue
-                if not dfs(neiNodes, node):
+            for neiN in adj[node]:
+                if not dfs(neiN, node):
                     return False
-            
             return True
-        return dfs(0, -1) and len(visit) == n 
         
+        return dfs(0, -1)
