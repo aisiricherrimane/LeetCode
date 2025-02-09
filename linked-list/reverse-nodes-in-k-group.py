9d2906eb8ae91bbec39ccfa60e5d33e763d95e89
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-09 14:37:05 -0500

    LC-reverse-nodes-in-k-group [Runtime: 0 ms; Memory: 18.4 MB]

--- linked-list/reverse-nodes-in-k-group.py
+++ linked-list/reverse-nodes-in-k-group.py
@@ -6,17 +6,19 @@
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         count = 0
+        count = 0
         curr = head
 
         while count < k and curr:
             curr = curr.next
             count += 1
-        
+
         if count == k:
             reversed_head = self.reverse(head, k)
             head.next = self.reverseKGroup(curr, k)
             return reversed_head
-        return head
+        else:
+            return head
     
     def reverse(self, node, k):
         new_head = None
@@ -27,7 +29,5 @@ class Solution:
             curr.next = new_head
             new_head = curr
             curr = temp
-
             k -= 1
         return new_head
-        
