daaca293c0c10636f347003f04cc91c5cfde6872
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-10 10:22:43 -0500

    LC-decode-ways [Runtime: N/A; Memory: N/A]

--- string/decode-ways.py
+++ string/decode-ways.py
@@ -7,6 +7,6 @@ class Solution:
                 dp[i] = 0
             else:
                 dp[i] = dp[i + 1]
-            if (i + 1) < len(s) and ((s[i] == '1'and s[i + 1] in '0123456789') or (s[i] == '2' and s[i + 1] in '0123456')):
+            if (i + 1) < len(s) and ((s[i] == '1' and s[i + 1] in '123456789') or (s[i] == '2' and s[i + 1] in '123456')):
                 dp[i] += dp[i + 2]
-        return dp[0]
+        return dp
