75db3557eda7bf3b59d861ba85572e807952869d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-09 22:39:17 -0500

    LC-min-cost-to-connect-all-points [Runtime: N/A; Memory: N/A]

--- array/min-cost-to-connect-all-points.py
+++ array/min-cost-to-connect-all-points.py
@@ -3,25 +3,30 @@ class Solution:
         n = len(points)
         adj = defaultdict(list)
 
-        for i in range(len(points)):
-            for j in range(i + 1, len(points)):
-                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
-                adj[i].append([j, dist])
-                adj[j].append([i, dist])
-
-        minH = [[0, 0]] # dist, 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append((j, dist))
+                adj[j].append((i, dist))
+        minH = [[0, 0]]
         visit = set()
         res = 0
-        
+
         while len(visit) < n:
             dist, p = heapq.heappop(minH)
 
             if p in visit:
                 continue
+            
             visit.add(p)
             res += dist
+            print(minH)
             for neiP, neid in adj[p]:
                 if neiP not in visit:
                     heapq.heappush(minH, [neid, neiP])
+
         return res
- 
+        
+               
