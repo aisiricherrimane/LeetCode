3f014b3ab31c7f2935c8ed092d377695a343e222
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-10 17:39:00 -0500

    LC-plates-between-candles [Runtime: 100 ms; Memory: 49.1 MB]

--- prefix-sum/plates-between-candles.py
+++ prefix-sum/plates-between-candles.py
@@ -1,35 +1,33 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        n = len(s)
-        res = []
-        prefix_plate = [0] * n
-        plates = 0
+        prefix = []
+        plate = 0
+        for i, v in enumerate(s):
+            if v == '*':
+                plate += 1
+            prefix.append(plate)
 
-        for i in range(n):
-            if s[i] == '*':
-                plates += 1
-            prefix_plate[i] = plates
-        
-        left_candle = [-1] * n
+        left_candle = [-1] * len(s)
         nearest = -1
-        for i in range(n):
+        for i in range(len(s)):
             if s[i] == '|':
                 nearest = i
             left_candle[i] = nearest
-        
-        right_candle = [-1] * n
+
+        right_candle = [-1] * len(s)
         nearest = -1
-        for i in range(n - 1, -1, -1):
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == '|':
                 nearest = i
             right_candle[i] = nearest
-        
-        for start, end in queries:
-            left_bound = right_candle[start]
-            right_bound = left_candle[end]
+
+        res = []
+        for l, r in queries:
+            left_bound = right_candle[l]
+            right_bound = left_candle[r]
 
             if left_bound != -1 and right_bound != -1 and left_bound < right_bound:
-                res.append(prefix_plate[right_bound] - prefix_plate[left_bound])
+                res.append(prefix[right_bound] - prefix[left_bound])
             else:
                 res.append(0)
         return res
