8fb146df05990a9a3f5c2dfe1cca2ad635b87cb2
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-10 15:16:00 -0500

    LC-jump-game-ii [Runtime: 10 ms; Memory: 18.7 MB]

--- dynamic-programming/jump-game-ii.py
+++ dynamic-programming/jump-game-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         curr_end = 0
-        curr_far = 0
         count = 0
+        curr_far = 0
 
         for i in range(len(nums) - 1):
             curr_far = max(curr_far, i + nums[i])
@@ -11,3 +11,5 @@ class Solution:
                 count += 1
                 curr_end = curr_far
         return count
+
+
