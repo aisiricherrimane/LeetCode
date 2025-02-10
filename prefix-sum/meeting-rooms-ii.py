324a757f270512b71e5f1acfc2a4e847a6950219
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-10 11:41:37 -0500

    LC-meeting-rooms-ii [Runtime: N/A; Memory: N/A]

--- prefix-sum/meeting-rooms-ii.py
+++ prefix-sum/meeting-rooms-ii.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(1, len(intervals)):
             prev_end = intervals[i - 1][1]
-            if prev_end < intervals[i][0]:
+            if prev_end <= intervals[i][0]:
                 count -= 1
         return count
         
