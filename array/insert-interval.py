2a1bb17681b0b4eb2cb0e07bc2dafa7c954f1eb9
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-10 11:30:08 -0500

    LC-insert-interval [Runtime: 2 ms; Memory: 19.7 MB]

--- array/insert-interval.py
+++ array/insert-interval.py
@@ -6,12 +6,15 @@ class Solution:
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 return res + intervals[i:]
-            
+
             elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
-
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1],intervals[i][1])]
+                newInterval = [min(newInterval[0], intervals[i][0]), max(intervals[i][1], newInterval[1])]
+        
         res.append(newInterval)
 
         return res
+
+
+        
