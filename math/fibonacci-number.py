4ea2b751ac26918f60fdb6f0b3f065c2b41214bd
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-10 07:57:04 -0500

    LC-fibonacci-number [Runtime: 342 ms; Memory: 17.9 MB]

--- math/fibonacci-number.py
+++ math/fibonacci-number.py
@@ -1,14 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        first=0
-        second =1
-        if n==0:
+
+        if n == 0:
             return 0
-        if n==1:
+        if n == 1:
             return 1
-        for i in range(2,n+1):
-            result=first + second
-            first=second
-            second= result
-        return result
-        
+        return self.fib(n - 1) + self.fib(n-2)
