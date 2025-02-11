1e1b5ad051032ee41ac5762476645d55ab0b785f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-11 13:49:54 -0500

    LC-basic-calculator [Runtime: 28 ms; Memory: 19 MB]

--- stack/basic-calculator.py
+++ stack/basic-calculator.py
@@ -1,39 +1,44 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        num = 0 
+        stack = [ ]
         sign = 1
+        num = 0
 
         for char in s:
             if char.isdigit():
                 num = num * 10 + int(char)
-            elif char == '+':
-                stack.append(num * sign)
-                num = 0
-                sign = 1
-            elif char == '-':
-                stack.append(num * sign)
-                num = 0
-                sign = -1
+
             elif char == '(':
                 stack.append(sign)
                 stack.append('(')
                 sign = 1
                 
+            elif char == '+':
+                stack.append(sign * num)
+                sign = 1
+                num = 0
+            elif char == '-':
+                stack.append(sign * num)
+                sign = -1
+                num = 0
             elif char == ')':
                 stack.append(num * sign)
                 num = 0
-
                 temp = 0
                 while stack and stack[-1] != '(':
                     temp += stack.pop()
-
                 stack.pop()
                 sign = stack.pop()
 
                 stack.append(sign * temp)
+        
         if num != 0:
             stack.append(sign * num)
         return sum(stack)
 
 
+            
+
+
+
+        
