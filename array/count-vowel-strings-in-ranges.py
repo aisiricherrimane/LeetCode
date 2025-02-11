1782adef77e21d1012d6d2d0731552c42e28af8c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-11 13:22:58 -0500

    LC-count-vowel-strings-in-ranges [Runtime: N/A; Memory: N/A]

--- array/count-vowel-strings-in-ranges.py
+++ array/count-vowel-strings-in-ranges.py
@@ -1,21 +1,11 @@
-from typing import List
-
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        prefix = [0] * (len(words) + 1)
+        temp = [0] * len(words)
 
-        for i, word in enumerate(words):
-            if word[0] in 'aeiou' and word[-1] in 'aeiou':
-                prefix[i + 1] = prefix[i] + 1
-            else:
-                prefix[i + 1] = prefix[i] 
-        
+        for i, w in enumerate(words):
+            if w[0] in 'aeiou' and w[-1] in'aeiou':
+                temp[i] = 1
         res = []
-
         for l, r in queries:
-            res.append(prefix[r + 1] - prefix[l])
-
+            res.append(sum(temp[l:r + 1]))
         return res
-
-        
-
