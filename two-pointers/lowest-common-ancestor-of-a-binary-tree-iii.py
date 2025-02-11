a320c2a5da4941f8c4d6c638ae4c3315af3d6afe
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-11 14:28:19 -0500

    LC-lowest-common-ancestor-of-a-binary-tree-iii [Runtime: 56 ms; Memory: 20.7 MB]

--- two-pointers/lowest-common-ancestor-of-a-binary-tree-iii.py
+++ two-pointers/lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,12 +10,11 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        p1, p2 = p, q
-        print(p1.val, p2.val)
+        p1 = p
+        p2 = q
         while p1 != p2:
             p1 = p1.parent if p1.parent else q
             p2 = p2.parent if p2.parent else p
-            print(p1.val, p2.val)
-
         return p1
+
         
