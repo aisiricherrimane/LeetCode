5670ea62eccfbd4fd29fe35e5a34953b434c9f76
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-11 12:56:42 -0500

    LC-analyze-user-website-visit-pattern [Runtime: 11 ms; Memory: 18.3 MB]

--- sorting/analyze-user-website-visit-pattern.py
+++ sorting/analyze-user-website-visit-pattern.py
@@ -3,12 +3,14 @@ class Solution:
         data = sorted(zip(timestamp, username, website))
 
         user_web = defaultdict(list)
+
         for t, u, w in data:
             user_web[u].append(w)
 
         web_user_count = defaultdict(int)
+
         for u, w in user_web.items():
-            for c in combinations(w, 3):
+            for c in set(combinations(w, 3)):
                 web_user_count[c] += 1
         
         res = sorted(web_user_count.keys(), key = lambda x:(-web_user_count[x], x))
