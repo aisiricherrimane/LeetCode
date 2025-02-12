e725e8154920dc30b42e37c57cb0ae3abba5577d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-12 17:17:32 -0500

    LC-walls-and-gates [Runtime: N/A; Memory: N/A]

--- matrix/walls-and-gates.py
+++ matrix/walls-and-gates.py
@@ -10,15 +10,21 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if rooms[r][c] == 0:
-                    q.append((r, c))
+                    q.append((r, c, 0))
 
-
+        visit = set()
 
         while q:
-            r, c = q.popleft()
-            for dr, dc in [[0, 1] , [0, -1] , [1, 0], [-1, 0]]:
-                nr, nc = dr + r, dc + c
-                if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == 2147483647:
-                    rooms[nr][nc] = rooms[r][c] + 1
-                    q.append((nr, nc))
-     
+            for _ in range(len(q)):
+                r, c, a = q.popleft()
+
+                if (r, c) in visit:
+                    return 
+                visit.add((r, c))
+                rooms[r][c] = a
+
+                for dr, dc in [[0, 1] , [0, -1] , [1, 0], [-1, 0]]:
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == 2147483647:
+                        q.append((nr, nc, a + 1))
+        return rooms
