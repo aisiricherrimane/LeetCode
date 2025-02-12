b572dfed5b6e8340e2c4084f70398223120bcd14
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-11 19:29:25 -0500

    LC-kth-largest-element-in-an-array [Runtime: 103 ms; Memory: 28.6 MB]

--- quickselect/kth-largest-element-in-an-array.py
+++ quickselect/kth-largest-element-in-an-array.py
@@ -2,23 +2,12 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-        def quickselect(l, r):
-            p = l
-            pivot = nums[r]
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
 
-            for i in range(l, r):
-                if nums[i] <= pivot:
-                    nums[i], nums[p] = nums[p], nums[i]
-                    p += 1
-            nums[p], nums[r] = nums[r], nums[p]
-
-            if p > k:
-                return quickselect(l, p - 1)
-
-            elif p < k:
-                return quickselect(p + 1, r)
-            
-            else:
-                return nums[p]
-        return quickselect(0, len(nums) - 1)
+        
