05781572a8313211210ec3c13f6d5928f078fc80
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-12 12:55:49 -0500

    LC-sort-colors [Runtime: 0 ms; Memory: 17.9 MB]

--- array/sort-colors.py
+++ array/sort-colors.py
@@ -9,14 +9,13 @@ class Solution:
 
         while i <= r:
             if nums[i] == 0:
-                nums[l], nums[i] = nums[i], nums[l]
+                nums[i], nums[l] = nums[l], nums[i]
                 l += 1
             elif nums[i] == 2:
                 nums[r], nums[i] = nums[i], nums[r]
                 r -= 1
                 i -= 1
             i += 1
-        return nums
+        
 
         
-    
