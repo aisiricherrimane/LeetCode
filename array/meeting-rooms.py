310a4c7a2d58cace32f799eb10761dc65d977fe8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-12 14:49:39 -0500

    LC-meeting-rooms [Runtime: 3 ms; Memory: 19.8 MB]

--- array/meeting-rooms.py
+++ array/meeting-rooms.py
@@ -2,10 +2,9 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
 
-        for i in range(1, len(intervals)):
-            prev_end = intervals[i - 1][1]
 
-            if prev_end > intervals[i][0]:
+        for i in range(1, len(intervals)):
+            
+            if intervals[i - 1][1] > intervals[i][0]:
                 return False
         return True
-        
