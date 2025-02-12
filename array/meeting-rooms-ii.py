298415b93a6f3bfaf4bf7ce09d7049fc2cc93b1c
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-12 13:56:48 -0500

    LC-meeting-rooms-ii [Runtime: 3 ms; Memory: 19.9 MB]

--- array/meeting-rooms-ii.py
+++ array/meeting-rooms-ii.py
@@ -1,14 +1,13 @@
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()  
-        rooms = []  
-        heapq.heappush(rooms, intervals[0][1]) 
+        intervals.sort(key = lambda x:x[0])
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
 
         for i in intervals[1:]:
-            heapq.heappush(rooms, i[1])
-            if rooms[0] <= i[0]:
-                heapq.heappop(rooms)  # Remove ended meeting from heap
-
-        return len(rooms)  #
-
+            heapq.heappush(heap, i[1])
+            if heap and heap[0] <= i[0]:
+                heapq.heappop(heap)
+        return len(heap)
+            
