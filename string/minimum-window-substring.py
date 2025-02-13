ad30e0b24569a3fe7994a36a0700e076a49d9132
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-13 07:46:14 -0500

    LC-minimum-window-substring [Runtime: 67 ms; Memory: 18.1 MB]

--- string/minimum-window-substring.py
+++ string/minimum-window-substring.py
@@ -1,34 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t:
-            return ''
-        countT = Counter(t)
+        count_t = Counter(t)
         window = {}
 
-        res = [-1, -1]
-        resl = float(inf)
-
+        need = len(count_t)
         have = 0
-        need = len(countT)
+
+        resl = float('inf')
+        resp = [-1, -1]
 
         l = 0
         for r in range(len(s)):
-            letter = s[r]
-            window[letter] = 1 + window.get(letter, 0)
+            char = s[r]
+            window[char] = 1 + window.get(char, 0)
 
-            if letter in countT and window[letter] == countT[letter]:
+            if char in count_t and window[char] == count_t[char]:
                 have += 1
             
             while have == need:
-                if (r - l + 1) < resl:
+                if resl > r - l + 1:
                     resl = r - l + 1
-                    res = [l , r]
+                    resp = [l, r]
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if s[l] in count_t and window[s[l]] < count_t[s[l]]:
                     have -= 1
                 l += 1
-        l, r = res
-        return s[l:r + 1]
-
-
-        
+        l, r = resp
+        return s[l:r+1]
