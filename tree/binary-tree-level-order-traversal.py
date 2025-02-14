ac8d12b62427ba93877db5a074f4a14b4e627ef8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-14 15:25:02 -0500

    LC-binary-tree-level-order-traversal [Runtime: N/A; Memory: N/A]

--- tree/binary-tree-level-order-traversal.py
+++ tree/binary-tree-level-order-traversal.py
@@ -19,7 +19,7 @@ class Solution:
                 if node:
                     q.append(node.left) if node.left else None
                     q.append(node.right) if node.left else None
-                l   evel.append(node.val)
+                    level.append(node.val)
             res.append(level)
         return res
 
