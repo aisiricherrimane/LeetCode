eb959011b733b0023dd74124e7b49d221713c1d8
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-14 18:02:41 -0500

    LC-jump-game [Runtime: 42 ms; Memory: 18.4 MB]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -3,9 +3,7 @@ class Solution:
         max_jump = 0
 
         for i, n in enumerate(nums):
-            if max_jump < i:
+            if i > max_jump:
                 return False
-            max_jump = max(max_jump, i + n)
-            if max_jump >= len(nums):
-                break
+            max_jump = max(max_jump, n + i)
         return True
