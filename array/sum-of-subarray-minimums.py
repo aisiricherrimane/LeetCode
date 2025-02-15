2f59332c7a738073fabda790344fa5e9db6d8bfc
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-15 08:17:55 -0500

    LC-sum-of-subarray-minimums [Runtime: 71 ms; Memory: 21.5 MB]

--- array/sum-of-subarray-minimums.py
+++ array/sum-of-subarray-minimums.py
@@ -1,17 +1,18 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        mod = 10**9 + 7
-        sum_min_subs = 0
+        MOD = 10 ** 9 + 7
         stack = []
+        sum_of_mins = 0 
 
         for i in range(len(arr) + 1):
-            while stack and (i == len(arr) or arr[stack[-1]] >= arr[i]):
+            while stack and ((i == len(arr))or (arr[stack[-1]] >= arr[i])):
                 mid = stack.pop()
-                left = -1 if not stack else stack[-1]
+                left = -1 if not stack else stack[-1] 
                 right = i
-                # number of subarrays
+
                 count = (mid - left) * (right - mid)
-                sum_min_subs += (count * arr[mid])
+                sum_of_mins += (arr[mid] * count)
             stack.append(i)
-        return sum_min_subs % mod
+        return sum_of_mins % MOD
+
         
