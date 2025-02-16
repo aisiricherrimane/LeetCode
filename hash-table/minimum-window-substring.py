8245ab3c8c861bf0bfb3060e78af489f1bdbd161
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-16 10:39:20 -0500

    LC-minimum-window-substring [Runtime: 71 ms; Memory: 18.2 MB]

--- hash-table/minimum-window-substring.py
+++ hash-table/minimum-window-substring.py
@@ -1,5 +1,7 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if not t:
+            return ''
         count_t = Counter(t)
         window = {}
 
@@ -7,23 +9,28 @@ class Solution:
         have = 0
 
         resl = float('inf')
-        resp = [-1, -1]
+        res = [-1, -1]
 
         l = 0
+
         for r in range(len(s)):
             char = s[r]
+
             window[char] = 1 + window.get(char, 0)
 
-            if char in count_t and window[char] == count_t[char]:
+            if char in count_t and count_t[char] == window[char]:
                 have += 1
-            
+
             while have == need:
-                if resl > r - l + 1:
+                if r - l + 1 < resl:
                     resl = r - l + 1
-                    resp = [l, r]
+                    res = [l, r]
                 window[s[l]] -= 1
+
                 if s[l] in count_t and window[s[l]] < count_t[s[l]]:
                     have -= 1
                 l += 1
-        l, r = resp
+        l, r = res
         return s[l:r+1]
+                    
+        
