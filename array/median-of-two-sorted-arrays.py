a41e62a7b1e338b79e528f5517ce2889ada91090
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-16 07:57:06 -0500

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- array/median-of-two-sorted-arrays.py
+++ array/median-of-two-sorted-arrays.py
@@ -1,32 +1,36 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
-        if len(B) < len(A):
-            A , B = B, A
+        A = nums1
+        B = nums2
 
-        total =  len(B) + len(A)
+        if len(B) < len(A):
+            A, B = B, A
+
+        total = len(A) + len(B)
         half = total // 2
-        l = 0 
+        l = 0
         r = len(A) - 1
 
         while True:
             i = (l + r) // 2
             j = half - i - 2
 
-            Aleft = A[i] if i >= 0 else float('-infinity')
-            Aright = A[i + 1] if (i + 1) < len(A) else float('infinity')
-            Bleft = B[j] if j >= 0 else float('-infinity')
-            Bright = B[j + 1] if (j + 1) < len(B) else float('infinity')
+            ALeft = A[i] if i >= 0 else float('-inf')
+            ARight = A[i + 1] if (i + 1) < len(A) else float('inf')
+            BLeft = B[j] if j >= 0 else float('-inf')
+            BRight = B[j + 1] if (j + 1)  >= 0 else float('inf')
 
-            if Aleft <= Bright and Bleft <= Aright:
-                # partition is correct
+            if ALeft <= BRight and BLeft <= BRight:
                 if total % 2 == 0:
-                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                    return (min(ARight, BRight) + max(ALeft, BLeft)) / 2
                 else:
-                    return min(Aright, Bright)
-            if Aleft > Bright:
+                    return min(ARight, BRight)
+            if ALeft >= BRight:
                 r = i - 1
             else:
                 l = i + 1
+                
+
+
 
         
