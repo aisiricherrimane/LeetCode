8776ced64644c40e688baacced575d654637703b
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-16 10:58:37 -0500

    LC-two-sum-ii-input-array-is-sorted [Runtime: 7 ms; Memory: 18.6 MB]

--- two-pointers/two-sum-ii-input-array-is-sorted.py
+++ two-pointers/two-sum-ii-input-array-is-sorted.py
@@ -6,8 +6,8 @@ class Solution:
         while l < r:
             if numbers[l] + numbers[r] == target:
                 return [l + 1, r + 1]
-            elif numbers[l] + numbers[r] > target:
-                r -= 1
-            else:
+            elif numbers[l] + numbers[r] < target:
                 l += 1
+            else:
+                r -= 1
         
