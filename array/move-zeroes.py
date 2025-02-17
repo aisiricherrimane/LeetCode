cba3b7f411e020ce5bfcdee677bc1cfe3156d7fb
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-16 21:24:34 -0500

    LC-move-zeroes [Runtime: 7 ms; Memory: 19 MB]

--- array/move-zeroes.py
+++ array/move-zeroes.py
@@ -3,12 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums) < 2:
-            return nums
         l = 0
-
         for r in range(len(nums)):
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-
+        
