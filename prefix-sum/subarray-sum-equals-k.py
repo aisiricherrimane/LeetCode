6154f4909ddec9a2e7a6d57d815040c461979bef
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 07:07:15 -0500

    LC-subarray-sum-equals-k [Runtime: 29 ms; Memory: 20.5 MB]

--- prefix-sum/subarray-sum-equals-k.py
+++ prefix-sum/subarray-sum-equals-k.py
@@ -1,16 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        temp = {0:1}
+        curr_num = 0
         subs = 0
-        sum_freq = {0:1}
-        curr_sum = 0
 
-        for num in nums:
-            curr_sum += num
-            print(f' c {curr_sum}')
-            if curr_sum - k in sum_freq:
-                print(sum_freq[curr_sum - k])
-                subs += sum_freq[curr_sum - k]
+        for i in range(len(nums)):
+            curr_num += nums[i]
+            if curr_num - k in temp:
+                subs += temp[curr_num - k]
             
-            sum_freq[curr_sum] = 1 + sum_freq.get(curr_sum, 0)
-        print(sum_freq)
+            temp[curr_num] = 1 + temp.get(curr_num, 0)
         return subs
+        
