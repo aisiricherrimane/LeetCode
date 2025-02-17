77a83e9cfebef773b271a4168d7970c85ef51a16
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 13:49:14 -0500

    LC-course-schedule-iv [Runtime: 36 ms; Memory: 20.5 MB]

--- topological-sort/course-schedule-iv.py
+++ topological-sort/course-schedule-iv.py
@@ -1,29 +1,23 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        adj = {i : [] for i in range(numCourses)}
-        
+        adj = {i:[] for i in range(numCourses)}
+
         for p, c in prerequisites:
             adj[c].append(p)
-        
+
         premap = defaultdict(set)
+
         def dfs(crs):
             if crs not in premap:
                 for c in adj[crs]:
                     premap[crs] |= dfs(c)
                 premap[crs].add(crs)
             return premap[crs]
-        
-        for crs in range(numCourses):
-            dfs(crs)
 
+        for c in range(numCourses):
+            dfs(c)
         res = []
         for p, c in queries:
             res.append(p in premap[c])
         return res
-
-        
-
-
-
-
         
