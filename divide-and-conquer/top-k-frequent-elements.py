57d70b853e154a6d71dbfc85668a06cb056e0791
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 12:16:45 -0500

    LC-top-k-frequent-elements [Runtime: 11 ms; Memory: 24.4 MB]

--- divide-and-conquer/top-k-frequent-elements.py
+++ divide-and-conquer/top-k-frequent-elements.py
@@ -1,21 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums)
-
         freq = [[] for i in range(len(nums) + 1)]
 
-        for n, f in count.items():
-            freq[f].append(n)
+        count = Counter(nums)
+        for n, c in count.items():
+            freq[c].append(n)
 
         res = []
+
         for i in range(len(freq) - 1, -1, -1):
             for n in freq[i]:
-                    res.append(n)
-                    k -= 1
-                    if k == 0:
-                        return res
-
-
-
+                res.append(n)
+                k -= 1
+                if  k == 0:
+                    return res
 
         
