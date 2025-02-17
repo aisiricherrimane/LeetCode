d977f587c443891bfe4bb0be5e4d455d61e353b0
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 13:57:11 -0500

    LC-course-schedule-ii [Runtime: 4 ms; Memory: 19.8 MB]

--- graph/course-schedule-ii.py
+++ graph/course-schedule-ii.py
@@ -2,7 +2,7 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adj = {i:[] for i in range(numCourses)}
 
-        for p, c in prerequisites:
+        for c, p in prerequisites:
             adj[c].append(p)
         
         res = []
@@ -10,10 +10,11 @@ class Solution:
         cycle = set()
 
         def dfs(crs):
-            if crs in cycle:
-                return False
             if crs in visited:
                 return True
+            if crs in cycle:
+                return False
+
             cycle.add(crs)
             for pre in adj[crs]:
                 if not dfs(pre):
@@ -21,9 +22,12 @@ class Solution:
             res.append(crs)
             cycle.remove(crs)
             visited.add(crs)
+            return True
 
         for c in range(numCourses):
-            dfs(c)
+            if not dfs(c):
+                return []
+                
         
         return res
         
