bc77afa7cde3e11236342614fc1d74807935794e
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 06:53:30 -0500

    LC-basic-calculator [Runtime: 30 ms; Memory: 19 MB]

--- string/basic-calculator.py
+++ string/basic-calculator.py
@@ -1,44 +1,45 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = [ ]
+        stack = []
+        curr_num = 0
         sign = 1
-        num = 0
 
-        for char in s:
+        for i in range(len(s)):
+            char = s[i]
             if char.isdigit():
-                num = num * 10 + int(char)
-
+                curr_num = curr_num * 10 + int(char)
+            
+            elif char == '+':
+                stack.append(sign*curr_num)
+                curr_num = 0
+                sign = 1
+            
+            elif char == '-':
+                stack.append(sign * curr_num)
+                curr_num = 0
+                sign = -1
+            
             elif char == '(':
                 stack.append(sign)
                 stack.append('(')
                 sign = 1
-                
-            elif char == '+':
-                stack.append(sign * num)
-                sign = 1
-                num = 0
-            elif char == '-':
-                stack.append(sign * num)
-                sign = -1
-                num = 0
+
             elif char == ')':
-                stack.append(num * sign)
-                num = 0
+                stack.append(sign * curr_num)
+                curr_num = 0
+                
                 temp = 0
-                while stack and stack[-1] != '(':
+                while stack[-1] != '(':
                     temp += stack.pop()
                 stack.pop()
                 sign = stack.pop()
 
                 stack.append(sign * temp)
         
-        if num != 0:
-            stack.append(sign * num)
+        if curr_num > 0:
+            stack.append(curr_num * sign)
+        
         return sum(stack)
-
-
-            
-
 
 
         
