34d7bbd63fe0aff54d65248c091e0ffaaec3d789
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 11:46:44 -0500

    LC-car-fleet [Runtime: N/A; Memory: N/A]

--- monotonic-stack/car-fleet.py
+++ monotonic-stack/car-fleet.py
@@ -1,7 +1,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position, speed))
-        times = [(target - p) / s for p, s in cars]
+        data = sorted(zip(position, speed))
+
+        times = [(target - p)// s for p, s in data]
         count = 0
 
         while len(times) > 1:
@@ -9,8 +10,8 @@ class Solution:
 
             if lead < times[-1]:
                 count += 1
+
             else:
                 times[-1] = lead
-        return  count +len(times)
+        return count + len(times)
         
-     
