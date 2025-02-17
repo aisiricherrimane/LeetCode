380cf2c71d056457152ad415f15cf754d2fb1a0d
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 07:52:52 -0500

    LC-analyze-user-website-visit-pattern [Runtime: 15 ms; Memory: 18.3 MB]

--- sorting/analyze-user-website-visit-pattern.py
+++ sorting/analyze-user-website-visit-pattern.py
@@ -1,26 +1,19 @@
-from collections import defaultdict
-from itertools import combinations
-from typing import List
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         data = sorted(zip(timestamp, username, website))
+        user_website = defaultdict(list)
 
-        user_web = defaultdict(list)
-
-        for t, u, w in data:
-            user_web[u].append(w)
-
-        web_user_count = defaultdict(int)
+        for _, u, w in data:
+            user_website[u].append(w)
         
-        for u, w in user_web.items():
-            seen = set()  # Avoid redundant counting per user
-            for c in combinations(w, 3):
-                if c not in seen:
-                    web_user_count[c] += 1
-                    seen.add(c)
+        websites_count = defaultdict(int)
+        for u, w in user_website.items():
+            for s in set(combinations(w, 3)):
+                websites_count[s] += 1
         
-        res = sorted(web_user_count.keys(), key = lambda x:(-web_user_count[x], x))
+        result = sorted(websites_count.keys(), key = lambda x:( -websites_count[x], x))
 
-        return list(res[0])
+        return list(result[0])
+
+
         
-       
