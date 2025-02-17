19fcd5af61e978c0e83abcf75825a5596d8efb95
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 12:26:20 -0500

    LC-all-nodes-distance-k-in-binary-tree [Runtime: 43 ms; Memory: 18.4 MB]

--- breadth-first-search/all-nodes-distance-k-in-binary-tree.py
+++ breadth-first-search/all-nodes-distance-k-in-binary-tree.py
@@ -7,30 +7,31 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-
-        def parent(node, par):
-            if node:
-                node.parent = par
-                parent(node.left, node)
-                parent(node.right, node)
-        parent(root, None)
-
-        answer = []
-        visited = set()
-        def closest(node, dist):
-            if not node or node in visited:
-                return 
-            visited.add(node)
-            if dist == k:
-                answer.append(node.val)
-                return
-            closest(node.parent, dist + 1)
-            closest(node.right, dist + 1)
-            closest(node.left, dist + 1)
-        closest(target, 0)
-
-        return answer
-            
-            
         
+        def set_parent(node, p):
+            if node:
+                node.parent = p
+                set_parent(node.left, node)
+                set_parent(node.right, node)
+        set_parent(root, None)
+
+        res = []
+        visited = set()
+
+        def dfs(node, k):
+            if not node or node in visited:
+                return
+            if k == 0:
+                res.append(node.val)
+                return
+            
+            visited.add(node)
+            dfs(node.parent, k - 1)
+            dfs(node.left, k - 1)
+            dfs(node.right, k - 1)
+        
+        dfs(target, k)
+
+        return res
+
         
