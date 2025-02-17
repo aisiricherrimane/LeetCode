861fa9175fb2cd57690e4b500d4deb8abde2467f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-17 11:47:00 -0500

    LC-car-fleet [Runtime: 164 ms; Memory: 41 MB]

--- stack/car-fleet.py
+++ stack/car-fleet.py
@@ -2,7 +2,7 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         data = sorted(zip(position, speed))
 
-        times = [(target - p)// s for p, s in data]
+        times = [(target - p)/ s for p, s in data]
         count = 0
 
         while len(times) > 1:
