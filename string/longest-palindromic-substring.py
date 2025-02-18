668c81bb94c594dea38dd2f404346135bc3ba75f
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-18 08:29:28 -0500

    LC-longest-palindromic-substring [Runtime: N/A; Memory: N/A]

--- string/longest-palindromic-substring.py
+++ string/longest-palindromic-substring.py
@@ -1,32 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = [-1, -1]
         resl = 0
-
+        res = [-1, -1]
+        
         for i in range(len(s)):
+            #odd
             l = r = i
             while l >= 0 and r < len(s):
                 if s[l] == s[r]:
-                    if resl < r - l + 1:
-                        resl = r - l + 1
+                    if (r - l + 1) > resl:
+                        resl = (r - l + 1)
                         res = [l, r]
-                    l -= 1
-                    r += 1
-                else:
-                    break
-            
+                l -= 1
+                r += 1
+
             l = i
             r = i + 1
+            
             while l >= 0 and r < len(s):
                 if s[l] == s[r]:
-                    if resl < r - l + 1:
-                        resl = r - l + 1
+                    if (r - l + 1) > resl:
+                        resl = (r - l + 1)
                         res = [l, r]
-                    l -= 1
-                    r += 1
-                else:
-                    break
+                l -= 1
+                r += 1
+                
         l, r = res
         return s[l:r + 1]
-
         
