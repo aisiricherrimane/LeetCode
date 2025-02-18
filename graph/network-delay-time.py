230e794433473e9d97639bc68d9b8381b727f876
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-18 14:11:48 -0500

    LC-network-delay-time [Runtime: 416 ms; Memory: 19.5 MB]

--- graph/network-delay-time.py
+++ graph/network-delay-time.py
@@ -1,25 +1,25 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj = collections.defaultdict(list)
+        adj = {i:[] for i in range(n + 1)}
 
         for s, d, w in times:
             adj[s].append((d, w))
-
-        minH = [(0, k)]
+        
+        heap = [(0, k)]
         visit = set()
         t = 0
-        while minH:
-            weight, des = heapq.heappop(minH)
+
+        while heap:
+            weight, des = heapq.heappop(heap)
+
             if des in visit:
                 continue
             visit.add(des)
             t = weight
 
-            for neiN, neiW in adj[des]:
-                if neiN not in visit:
-                    heapq.heappush(minH, [weight+neiW, neiN])
+            for neiD, neiW in adj[des]:
+                heapq.heappush(heap, [neiW + weight, neiD])
         return t if len(visit) == n else -1
-
-
+        
 
         
