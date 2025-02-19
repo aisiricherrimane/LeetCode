974d92bf67cdb65fc0172cdab7727858a51c27cf
aisiri cherrimane narendra <119715750+aisiricherrimane@users.noreply.github.com>
2025-02-19 09:21:05 -0500

    LC-palindrome-partitioning [Runtime: 51 ms; Memory: 34.5 MB]

--- string/palindrome-partitioning.py
+++ string/palindrome-partitioning.py
@@ -1,18 +1,21 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        part = []
+        sub = []
 
         def dfs(i):
-            if i >= len(s):
-                res.append(part[:])
-                return
+            if i == len(s):
+                res.append(sub[:])
+                return 
             for j in range(i, len(s)):
                 if self.pali(s[i:j + 1]):
-                    part.append(s[i:j + 1])
+                    sub.append(s[i:j + 1])
                     dfs(j + 1)
-                    part.pop()
+                    sub.pop()
         dfs(0)
         return res
+    
+
     def pali(self, c):
         return c == c[::-1]
+        
